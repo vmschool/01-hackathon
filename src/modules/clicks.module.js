c710fabf7218d9638894e0326e30c9c6d9673bcd
Рыбников Андрей Анатольевич <43681241+egolegegit4226@users.noreply.github.com>
2021-08-21 02:06:41 +0700

    fist commit

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -1,5 +1,42 @@
-import {Module} from '../core/module'
+import { Module } from '../core/module'
 
 export class ClicksModule extends Module {
+  constructor(type, text, time_count) {
+    super(type, text)
+    this.time_count = time_count
 
+    // document.body.addEventListener('contextmenu', (event) => {
+    //   event.preventDefault()
+
+    //   if (event.target === document.body) {
+    //     this.trigger()
+    //   }
+    // })
+  }
+
+  trigger() {
+    console.log('start')
+
+    function countClicks() {
+      countClick += 1
+      console.log('countClick:', countClick)
+    }
+
+    let countClick = 0
+
+    document.body.addEventListener('click', countClicks)
+    document.body.addEventListener('dblclick', countClicks)
+
+    setTimeout(() => {
+      document.body.removeEventListener('click', countClicks)
+      document.body.removeEventListener('dblclick', countClicks)
+      console.log('stop');
+    }, this.time_count)
+
+    return countClick
+  }
+
+  toHTML() {
+    return `<li class="menu-item" data-type="${this.type}">${this.text}</li>`
+  }
 }
