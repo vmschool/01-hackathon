be42c4662e815af0071105367ed651a17db6792d
Anna Noskova <anni4anny@gmail.com>
2021-08-20 21:14:12 +0300

    Implemented message module

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -1,27 +1,28 @@
-import {Module} from '../core/module'
+import { Module } from "../core/module";
 
 export class ClicksModule extends Module {
-    constructor() {
-        super('click analytics', text) 
-    }
+  constructor() {
+    super("click analytics", text);
+  }
 
-    trigger() {
-        let clicksCounter = 0
-        document.addEventListener('click', (event) => {
-            const { target } = event
-            if (target) {
-                clicksCounter++
-            }
-        })
-        const currentTimeout = 20000
-        setTimeout(() => {
-            this.text = `Количество кликов за ${currentTimeout / 1000} секунд равно ${clicksCounter}`
-        }, currentTimeout)
+  trigger() {
+    let clicksCounter = 0;
+    document.addEventListener("click", (event) => {
+      const { target } = event;
+      if (target) {
+        clicksCounter++;
+      }
+    });
+    const currentTimeout = 20000;
+    setTimeout(() => {
+      const clicksAmount = `Количество кликов за ${
+        currentTimeout / 1000
+      } секунд равно ${clicksCounter}`;
+      const clicksAmountElement = document.createElement("div");
+      clicksAmountElement.textContent = clicksAmount;
+      
+    }, currentTimeout);
 
-        return clicksCounter
-    }
-
-    toHTML() {
-        return `<li class="menu-item" data-type="${this.type}">${this.text}</li>`
-    }
+    return clicksCounter;
+  }
 }
---
+++ src/modules/message.module.js
@@ -0,0 +1,53 @@
+import { Module } from "../core/module";
+
+export class MessageModule extends Module {
+  constructor() {
+    super("custom message", text);
+  }
+
+  trigger() {
+    COMMENTS_URL = "https://jsonplaceholder.typicode.com/comments";
+
+    const messageContainer = document.createElement("div");
+    messageContainer.className = "message-container";
+    document.querySelector("body").append(messageContainer);
+
+    function createRandomMessage() {
+      const randomId = Math.floor(Math.random() * 501) + 1;
+      const randomMessage = fetch(`${COMMENTS_URL}?id=${randomId}`)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Ошибка запроса!");
+          }
+          return response.json();
+        })
+        .then((comments) => {
+          const customMessage = document.createElement("div");
+          customMessage.textContent = comments[0].name;
+          messageContainer.append(customMessage);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+
+    function createMessageByInterval() {
+      const creator = setInterval(() => {
+        messageContainer.innerHTML = "";
+        createRandomMessage();
+      }, 2000);
+      return creator;
+    }
+
+    let intervalCreator1 = createMessageByInterval();
+
+    document.addEventListener("keydown", (event) => {
+      const { key } = event;
+      if (key === "Escape") {
+        clearInterval(intervalCreator1);
+      } else if (key === "Enter") {
+        intervalCreator1 = createMessageByInterval();
+      }
+    });
+  }
+}
