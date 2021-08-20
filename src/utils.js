3d3a5e2e6e79c860d696093cc82936287e8968ed
Denis Bugreev <den9.rcq@gmail.com>
2021-08-20 17:24:30 +0300

    create func utils

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,10 @@
 export function random(min, max) {
   return Math.round(min - 0.5 + Math.random() * (max - min + 1))
 }
+
+export function createArea() {
+  const area = document.createElement('div')
+  area.classList.add('area')
+  area.id = 'area'
+  document.body.append(area)
+}
