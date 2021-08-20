1855823e48b568b8c7bfa3a81efd009cfd53d7ba
sacden <sacden2009@gmail.com>
2021-08-20 14:09:55 +0200

    add a custom right-click menu

--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,34 @@
-import {Menu} from './core/menu'
+import { Menu } from "./core/menu";
 
 export class ContextMenu extends Menu {
+  constructor(selector) {
+    super(selector);
+    this.modules = ["Поменять цвет", "Создать фигуру", "Кастомное сообщение", "Собственный модуль"];
+  }
 
+  close() {
+    document.getElementById("menu").style.display = "none";
+  }
+
+  open(e) {
+    e.preventDefault();
+    if (document.getElementById("menu").style.display == "block") close();
+    else {
+      let newmenu = document.getElementById("menu");
+      newmenu.style.display = "block";
+      newmenu.style.left = e.pageX + "px";
+      newmenu.style.top = e.pageY + "px";
+    }
+  }
+  render() {
+    const getMenu = document.querySelector("#menu");
+    const getElements = this.modules.map((el) => `<li class="menu-item">${el}</li>`);
+    getMenu.insertAdjacentHTML("afterbegin", getElements.join(""));
+
+    document.onclick = this.close;
+    document.oncontextmenu = this.open;
+  }
 }
+
+export const menu = new ContextMenu();
+menu.render();
