74404f7e6655fa876c3be92cc158dff6df899333
Evgeny_Bobkov <freeedayy@gmail.com>
2021-08-20 21:47:59 +1000

    implemented menu basic function (v. 1.0)

--- src/app.js
+++ src/app.js
@@ -1,2 +1,14 @@
-import './styles.css'
+"use strict";
 
+import "./styles.css";
+import ContextMenu from "./menu";
+
+const contextMenu = new ContextMenu();
+
+document.body.addEventListener("contextmenu", (event) => {
+	contextMenu.open(event);
+});
+
+document.body.addEventListener("click", (event) => {
+	contextMenu.close(event);
+});
