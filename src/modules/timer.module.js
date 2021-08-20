2f36f361a15619d482862bf6f10a9986b06e02ae
Evgeny_Bobkov <freeedayy@gmail.com>
2021-08-20 22:58:03 +1000

    refactored menu (v. 1.1)

--- src/modules/timer.module.js
+++ src/modules/timer.module.js
@@ -1,5 +1,11 @@
-import {Module} from "@/core/module";
+import { Module } from "@/core/module";
 
 export default class Timer extends Module {
+	constructor(type, text) {
+		super(type, text);
+	}
 
+	trigger() {
+		console.log("Таймер запущен (тестовое сообщение)");
+	}
 }
