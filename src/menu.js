953be8835b848d7c416a14db7e1154690798419f
DmitryLavrov <d3236562@mail.ru>
2021-08-20 21:30:23 +0300

    Context menu implemented

--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,44 @@
 import {Menu} from './core/menu'
 
 export class ContextMenu extends Menu {
+  modules
+  contextMenu$
 
+  constructor(selector) {
+    super(selector)
+
+    this.modules = []
+    this.contextMenu$ = document.querySelector('#menu')
+
+    document.body.addEventListener('contextmenu', this.open.bind(this))
+    this.contextMenu$.addEventListener('click', this.triggerModule.bind(this))
+  }
+
+  open(event) {
+    event.preventDefault()
+    this.contextMenu$.style.left = event.pageX + 'px'
+    this.contextMenu$.style.top = event.pageY + 'px'
+    this.contextMenu$.style.display = 'block'
+  }
+
+  close() {
+    this.contextMenu$.style.display = 'none'
+  }
+
+  add(module) {
+    this.modules.push(module)
+    this.contextMenu$.insertAdjacentHTML('beforeend', module.toHTML())
+  }
+
+  triggerModule(event) {
+    const menuItem = event.target.closest('.menu-item')
+
+    this.modules.forEach(module => {
+      if (module.type === menuItem.dataset.type) {
+        module.trigger()
+      }
+    })
+
+    this.close()
+  }
 }
