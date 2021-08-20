b30e3cb1dc5f3329a037f8ab4b25e56ecf4f059f
Anna Noskova <anni4anny@gmail.com>
2021-08-20 19:49:04 +0300

    Implemented click analitics module

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -1,5 +1,27 @@
 import {Module} from '../core/module'
 
 export class ClicksModule extends Module {
+    constructor() {
+        super('click analytics', text) 
+    }
 
+    trigger() {
+        let clicksCounter = 0
+        document.addEventListener('click', (event) => {
+            const { target } = event
+            if (target) {
+                clicksCounter++
+            }
+        })
+        const currentTimeout = 20000
+        setTimeout(() => {
+            this.text = `Количество кликов за ${currentTimeout / 1000} секунд равно ${clicksCounter}`
+        }, currentTimeout)
+
+        return clicksCounter
+    }
+
+    toHTML() {
+        return `<li class="menu-item" data-type="${this.type}">${this.text}</li>`
+    }
 }
