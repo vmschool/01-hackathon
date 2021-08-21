5099ceb28103b6576653eb95a92f0835888e1743
Leetvig <leetvig@gmail.com>
2021-08-21 17:20:22 +0500

    Implemented background moduletest

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,5 +1,15 @@
-import {Module} from '../core/module'
+import { Module } from '../core/module';
+import { random } from '../utils';
 
 export class BackgroundModule extends Module {
+  constructor() {
+    super('background-color', 'Поменять текст');
+  }
 
+  trigger() {
+    const red = random(0, 255);
+    const green = random(0, 255);
+    const blue = random(0, 255);
+    document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+  }
 }
