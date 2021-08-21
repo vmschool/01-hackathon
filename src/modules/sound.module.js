9204aa102322eb2b51d556401a42f3c2a02721ff
Valeriy Harlamov <valera545@gmail.com>
2021-08-21 13:44:10 +0300

    Sound trouble

--- src/modules/sound.module.js
+++ src/modules/sound.module.js
@@ -1,9 +1,35 @@
 import { Module } from '../core/module';
 import { sounds } from '../db/sounds';
+import { random } from '../utils'
 
 export class SoundModule extends Module {
+  getSoundSrc() {
+    const soundName = sounds[random(0, sounds.length - 1)]
+    return `../assets/sounds/${soundName}.mp3`
+  }
+
+  /* createAudio() {
+    const audio = document.createElement('audio') 
+    audio.src = this.getSoundSrc()
+    audio.setAttribute('autoplay', '')
+    audio.textContent = 'Тег audio не поддерживается вашим браузером.'
+    return audio
+  }
 
   trigger() {
-    console.log('test');
+    if (document.querySelector('audio')) {
+      document.querySelector('audio').remove()
+    }
+    document.body.append(this.createAudio())
+  } */
+
+  createSound() {
+    const audio = new Audio(this.getSoundSrc()) 
+    audio.play()
   }
-}
+  
+  trigger() {
+      this.createSound()
+  }
+} 
+
