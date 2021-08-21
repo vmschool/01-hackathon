55291a9b013a8ddf5f67477699458cff6943de60
Pavel Koryakin <unclekoin@gmail.com>
2021-08-21 05:57:00 +0300

    feat: remove modal window

--- src/modules/test.module.js
+++ src/modules/test.module.js
@@ -1,11 +1,8 @@
 import { Module } from '../core/module';
-import ModalWindow from '../components/modal-window';
 
 export class TestModule extends Module {
 
   trigger() {
-    const modal = new ModalWindow('text');
     console.log('test');
-    modal.open();
   }
 }
