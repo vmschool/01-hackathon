dbf62e24950cb64221ad71e7cecdb7bb2198e9cd
shutnikPk <51722234+shutnikPk@users.noreply.github.com>
2021-08-21 09:38:52 +0300

    create customMessage.module

--- src/app.js
+++ src/app.js
@@ -5,30 +5,35 @@ import { createArea } from "./utils";
 import { BackgroundModule } from "./modules/background.module";
 import { RandomSound } from "./modules/randomSound.module";
 
-// Initialising contextMenu
-const contextMenu = new ContextMenu('#menu');
+import CustomMessage from "./modules/customMessage.module";
 
+// Initialising contextMenu
+const contextMenu = new ContextMenu("#menu");
 
 // creating field area, to generate objects there
 createArea();
 
-const area = document.querySelector('.area');
+const area = document.querySelector(".area");
 
 // чтобы только на квадрате меню открывалось
 area.addEventListener("contextmenu", (event) => {
-	contextMenu.open(event);
+  contextMenu.open(event);
 });
 document.body.addEventListener("click", (event) => {
-	contextMenu.close(event);
+  contextMenu.close(event);
 });
 
 // init modules
-const clickModule = new ClicksModule('clickModule', 'Counts clicks made in 3 seconds');
-const backgroundModule = new BackgroundModule()
-const randomSound = new RandomSound()
+const clickModule = new ClicksModule(
+  "clickModule",
+  "Counts clicks made in 3 seconds"
+);
+const backgroundModule = new BackgroundModule();
+const randomSound = new RandomSound();
+const customMessage = new CustomMessage();
 
 // add modules to contextMenu
 contextMenu.add(clickModule);
-contextMenu.add(backgroundModule)
-contextMenu.add(randomSound)
-
+contextMenu.add(backgroundModule);
+contextMenu.add(randomSound);
+contextMenu.add(customMessage);
