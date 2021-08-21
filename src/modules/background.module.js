7440062b12354b1c16d944fd03923d1e73bd5cc8
Yuri Simonov <yura15-96@mail.ru>
2021-08-21 12:48:55 +0300

    модуль случайного цвета для фона

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,5 +1,15 @@
-import {Module} from '../core/module'
+import { Module } from '../core/module'
 
 export class BackgroundModule extends Module {
+	constructor() {
+		super('background', 'Случайный цвет фона');
+	}
 
+	trigger() {
+		document.body.addEventListener('click', () => {
+			const randomColor = '#' + (Math.random().toString(16) + '000000').substring(2, 8).toUpperCase();
+			document.body.style.backgroundColor = randomColor;
+		})
+	}
 }
+
