c8ac5f6b66728a5fcc0d2bb6672f68feac2e2324
VitaliyOsin <d_dva@list.ru>
2021-08-21 10:40:26 +0700

    Первая реализация module.message

--- src/utils.js
+++ src/utils.js
@@ -4,11 +4,19 @@ export function random(min, max) {
 
 export function getRandomColor() {
   let r = `${(Math.floor(Math.random() * 256) + 1).toString(16).toUpperCase()}`
-  if (r.length === 1) r = `0${r}`
+  if (r.length === 1) r = `0${r}`;
   let g = (Math.floor(Math.random() * 256) + 1).toString(16).toUpperCase()
-  if (g.length === 1) g = `0${g}`
+  if (g.length === 1) g = `0${g}`;
   let b = (Math.floor(Math.random() * 256) + 1).toString(16).toUpperCase()
-  if (b.length === 1) b = `0${b}`
+  if (b.length === 1) b = `0${b}`;
 
-  return `#${r}${g}${b}`
+  return `#${r}${g}${b}`;
+}
+
+export const cel = el => document.createElement(el)
+export const qel = selector => document.querySelector(selector)
+export const setStyle = (el, styles) => {
+  for (let st in styles) {
+    el.style[st] = styles[st]
+  }
 }
