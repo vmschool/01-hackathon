a66cb243af018ae43d1de72b621485b7892b2169
Рыбников Андрей Анатольевич <43681241+egolegegit4226@users.noreply.github.com>
2021-08-21 20:29:19 +0700

    add function output result

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -1,23 +1,37 @@
 import { Module } from '../core/module'
+import { cel, qel, setStyle } from '../utils'
 
 export class ClicksModule extends Module {
   #countClick
 
   #stateCount
 
+  #moduleStyle
+
   constructor(type, text) {
     super(type, text)
     this.time_count = 3000
     this.#countClick = 0
     this.#stateCount = true
+    this.#moduleStyle = []
+  }
+
+  #showClick(event) {
+    const messageBlock = cel('div')
+    messageBlock.textContent = `Total clicks/dblclicks: ${this.#countClick}`
+    setStyle(messageBlock, this.#moduleStyle)
   }
 
   #countClicks(event) {
     if (this.#stateCount) {
+      this.#showClick(event)
+
       this.#countClick += 1
       console.log('countClick:', this.#countClick, 'event.type:', event.type)
     }
   }
+
+  #showResult() {}
 
   trigger() {
     console.log('start')
