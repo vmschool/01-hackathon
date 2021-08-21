0a5c663c224cfe999c463186399ba3a35686c533
shutnikPk <51722234+shutnikPk@users.noreply.github.com>
2021-08-21 17:44:41 +0300

    fix initialization Custom message

--- src/modules/timer.module.js
+++ src/modules/timer.module.js
@@ -1,8 +1,19 @@
 import { Module } from "@/core/module";
+import * as utils from "../utils";
 
 export default class Timer extends Module {
 	constructor(type, text) {
 		super(type, text);
+		this.time=10
+	}
+
+	timeOut() {
+		this.time--
+	}
+
+	renderBlock(){
+		const area = utils.getArea()
+		const blockColor = utils.getRandomColor()
 	}
 
 	trigger() {
