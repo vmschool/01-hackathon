f252f3f52b7f96a98d40767560c81be8e74f26b6
Evgeny_Bobkov <freeedayy@gmail.com>
2021-08-22 03:04:48 +1000

    palindrome refactored + createModal util (v1.0)

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,4 @@
+"use strict";
 // variables
 export let counter = -1;
 export const timer = 3;
@@ -54,4 +55,41 @@ function getRandomHex() {
 export function isPalindrome(str) {
 	if (!str || str.length < 3) return "empty or short string given";
 	return str === str.split("").reverse().join("") ? true : false;
+}
+
+export function createModal(modalId) {
+	const modal = document.createElement("div");
+	modal.id = `${modalId}`;
+	modal.style.display = "flex";
+	modal.style.flexDirection = "column";
+	modal.style.justifyContent = "center";
+	modal.style.alignItems = "center";
+	modal.style.padding = "15px";
+	modal.style.margin = "30% auto";
+	modal.style.height = "180px";
+	modal.style.width = "38%";
+	modal.style.background = "rgba(0, 0, 0, 0.3)";
+	modal.style.borderRadius = "10px";
+	modal.style.boxShadow = "0px 5px 10px 2px rgba(34, 60, 80, 0.2)";
+	modal.style.transition = "all 350ms ease";
+
+	const modalCloseButton = document.createElement("button");
+	modalCloseButton.textContent = `\u2716`;
+	modalCloseButton.style.cursor = "pointer";
+	modalCloseButton.style.display = "block";
+	modalCloseButton.style.position = "absolute";
+	modalCloseButton.style.left = "70%";
+	modalCloseButton.style.top = "30%";
+	modalCloseButton.style.fontSize = "15px";
+	modalCloseButton.style.borderStyle = "none";
+	modalCloseButton.style.background = "none";
+
+	modalCloseButton.addEventListener("click", () => {
+		const modal = document.querySelector(`#${modalId}`);
+		modal.innerHTML = "";
+		modal.remove();
+	});
+
+	modal.append(modalCloseButton);
+	return modal;
 }
