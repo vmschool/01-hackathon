49b3dfe7e3935aceaddf80745b4c9dc624ca8928
Denis Sitnikov <dmrock@Deniss-iMac.local>
2021-08-21 15:07:32 +0300

    Implemented menu

--- src/app.js
+++ src/app.js
@@ -1,2 +1,7 @@
 import './styles.css'
+import {ContextMenu} from "@/menu"
+import {TestModule} from "@/modules/test.module"
 
+const contextMenu = new ContextMenu("#menu")
+
+contextMenu.add(new TestModule('test', 'Тест'))
--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,36 @@
-import {Menu} from './core/menu'
+import {Menu} from './core/menu';
+import {Module} from "@/core/module";
 
 export class ContextMenu extends Menu {
+    constructor(selector) {
+        super(selector)
+        document.addEventListener('contextmenu', (event) => {
+            event.preventDefault()
+            this.open(event.clientY, event.clientX)
+        }, false)
+        this.el.addEventListener('click', (event) => {
+            console.log(event)
+            console.log(this.obj)
+            const type = event.target.dataset.type
+            console.log(type)
+            this.obj[type].trigger()
+        })
+    }
 
+    obj = {}
+
+    open(y, x) {
+        this.el.classList.add('open')
+        this.el.style.left = (x - 10) + "px"
+        this.el.style.top = (y - 10) + "px"
+    }
+
+    close() {
+        this.el.classList.remove('open')
+    }
+
+    add(item) {
+        this.el.insertAdjacentHTML('afterbegin', item.toHTML())
+        this.obj[item.type] = item
+    }
 }
