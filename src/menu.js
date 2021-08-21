a24a15faf3befd95da591ca0d537f9a9fbc0fcd6
Gurev Aleksey <gurevaleksey@yandex.ru>
2021-08-22 00:07:12 +0300

    Добавил работу меню

--- src/menu.js
+++ src/menu.js
@@ -1,32 +1,36 @@
 import { Menu } from "./core/menu";
-import * as settings from "./constants/settings";
 import { menuPosition } from "./utils";
 
 export class ContextMenu extends Menu {
   constructor(selector) {
     super(selector);
-    this.add();
+    this.modules = [];
     document.addEventListener("contextmenu", (event) => {
       event.preventDefault();
       this.open(event);
     });
   }
-
   open(event) {
     menuPosition(event, this.el);
     this.el.classList.add("menu", "open");
+    this.el.addEventListener("click", (event) => {
+      let { target } = event;
+      let datasetModule = target.dataset.type;
+      this.modules.forEach((element) => {
+        if (datasetModule === element.type) {
+          element.trigger();
+        }
+        this.close();
+      });
+    });
   }
 
   close() {
     this.el.classList.remove("open");
   }
 
-  add() {
-    settings.modules.forEach((element) => {
-      let module = document.createElement("li");
-      module.textContent = element;
-      module.classList.add("menu-item");
-      this.el.append(module);
-    });
+  add(module) {
+    this.modules.push(module);
+    this.el.innerHTML += module.toHTML();
   }
 }
