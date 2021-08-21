8c59cffcfa8151f5ba8f89fb3f7b75c7f8fa87c2
VitaliyOsin <d_dva@list.ru>
2021-08-21 19:55:59 +0700

    21-08-2021 19:55

--- src/modules/timer.module.js
+++ src/modules/timer.module.js
@@ -4,6 +4,7 @@ import { cel, qel, setStyle } from '../utils'
 export class TimerModule extends Module {
   constructor(type,text){
     super(type,text)
+    this.div = cel('div')
     this.styles = {
       div: {
         display: 'flex',
@@ -55,9 +56,8 @@ export class TimerModule extends Module {
   }
 
   #createTimer(point){
-    const div = cel('div')
-    setStyle(div, this.styles.div)
-    div.id = 'tmr'
+    setStyle(this.div, this.styles.div)
+    this.div.id = 'tmr'
 
     const input = cel('input');
     setStyle(input, this.styles.input)
@@ -68,24 +68,22 @@ export class TimerModule extends Module {
     const startBtn = cel('button')
     setStyle(startBtn, this.styles.button)
     startBtn.textContent = 'Пуск'
-    startBtn.addEventListener('click', this.#startTimer.bind(this, this.#closeTimer))
+    startBtn.addEventListener('click', () => {this.#startTimer(input)})
 
     const closeBtn = cel('button')
     setStyle(closeBtn, this.styles.button)
     closeBtn.textContent = 'Закрыть'
-    closeBtn.addEventListener('click', this.#closeTimer)
+    closeBtn.addEventListener('click', () => {this.#closeTimer(this.div)})
 
-    div.append(input, startBtn, closeBtn)
-    point.append(div)
+    this.div.append(input, startBtn, closeBtn)
+    point.append(this.div)
   }
 
-  #startTimer(closeFn){
-    const point = qel('#tmr')
-    let t = parseInt(qel('#tmr input').value)
-    point.innerHTML = ''
+  #startTimer(input){
+    let t = parseInt(input.value)
+    this.div.innerHTML = ''
     const h3 = cel('h1')
     h3.innerText = t
-    const underDiv = cel('div')
 
     const timeCounter = setInterval(() => {
       if(t >= 0){
@@ -98,16 +96,16 @@ export class TimerModule extends Module {
       }
       if(t === -5) {
         clearInterval(timeCounter)
-        closeFn()
+        this.#closeTimer(this.div)
       }
 
     }, 1000)
 
-    point.append(h3, underDiv)
+    this.div.append(h3)
   }
 
-  #closeTimer(){
-    qel('#tmr').remove()
+  #closeTimer(el){
+    el.remove()
   }
 
 }
