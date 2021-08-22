7f20ee18717f772569e156e7daf55a8a1a233f0b
Рыбников Андрей Анатольевич <43681241+egolegegit4226@users.noreply.github.com>
2021-08-22 12:34:15 +0700

    fix error context runtime

--- src/utils.js
+++ src/utils.js
@@ -4,27 +4,26 @@ export function random(min, max) {
 
 export function getRandomColor() {
   let r = `${(Math.floor(Math.random() * 256) + 1).toString(16).toUpperCase()}`
-  if (r.length === 1) r = `0${r}`;
+  if (r.length === 1) r = `0${r}`
   let g = (Math.floor(Math.random() * 256) + 1).toString(16).toUpperCase()
-  if (g.length === 1) g = `0${g}`;
+  if (g.length === 1) g = `0${g}`
   let b = (Math.floor(Math.random() * 256) + 1).toString(16).toUpperCase()
-  if (b.length === 1) b = `0${b}`;
+  if (b.length === 1) b = `0${b}`
 
-  return `#${r}${g}${b}`;
+  return `#${r}${g}${b}`
 }
 
-export const cel = el => document.createElement(el)
-export const qel = selector => document.querySelector(selector)
+export const cel = (el) => document.createElement(el)
+export const qel = (selector) => document.querySelector(selector)
 export const setStyle = (el, styles) => {
   for (let st in styles) {
     el.style[st] = styles[st]
   }
-
 }
 
 /* крайние правые и крайние нижние позиции мыши еще некоррктно вычисляются. нужна поправка */
 
-export function xyPopup(event) {
+export function xyTooltips(event) {
   const { width, height } = document.body.getBoundingClientRect()
   const delta = 58 / 2
   const pageX = event.pageX
@@ -35,14 +34,17 @@ export function xyPopup(event) {
   if (pageX < delta) {
     left = 0
   } else if (pageX + delta > width) {
-    left = width - 58 - 4
+    left = width - 58
+  } else if (width - pageX < 58) {
+    left = width - 58
   } else {
-    left = event.pageX + delta
+    left = pageX - delta
   }
+
   if (pageY < delta) {
     top = 0
   } else if (pageY + delta > height) {
-    top = height - 58 - 4
+    top = height - 58
   } else {
     top = event.pageY + delta
   }
