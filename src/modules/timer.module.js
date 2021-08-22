c5b2c901a8ab3904a5eb5f689d187583a60e8cbf
Mikhail Lapitskiy <hiimisha@mail.ru>
2021-08-22 19:37:34 +0300

    fix: timer err module

--- src/modules/timer.module.js
+++ src/modules/timer.module.js
@@ -3,40 +3,70 @@ import {Module} from '../core/module'
 export class TimerModule extends Module {
   constructor(type, text) {
     super(type, text);
+    this.flagTimerWork = false
   }
 
   trigger () {
-    const time = prompt('Введите время для таймера в формате hh.mm.ss')
-    const arrNumbersTime = time.split('.').map(Number)
-
-    if (arrNumbersTime.includes(NaN)) {
-      alert('Неверный формат данных')
+    if (this.flagTimerWork)  {
       return
-    }
+    } else {
+      const time = prompt('Введите время для таймера в формате hh.mm.ss (до 23.59.59)')
+      const arrNumbersTime = time.split('.').map(Number)
 
-    const divHours = document.getElementById("timer-hours")
-    const divMins = document.getElementById("timer-mins")
-    const divSecs = document.getElementById("timer-secs")
-    let endDate = arrNumbersTime[2] * 1000 + arrNumbersTime[1] * 60 * 1000 + arrNumbersTime[0] * 60 * 60 * 1000
-
-    const timer = setInterval(() => {
-      if (endDate >= 0) {
-        let hours = Math.floor((endDate % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-        let mins = Math.floor((endDate % (1000 * 60 * 60)) / (1000 * 60))
-        let secs = Math.floor((endDate % (1000 * 60)) / 1000)
-
-        divHours.innerHTML = ("0"+hours).slice(-2)
-        divMins.innerHTML = ("0"+mins).slice(-2)
-        divSecs.innerHTML = ("0"+secs).slice(-2)
-
-        endDate = endDate - 1000
-      } else {
-        alert('Таймер закончил свою работу')
-        divHours.innerHTML = ''
-        divMins.innerHTML = ''
-        divSecs.innerHTML = ''
-        clearInterval(timer)
+      if (arrNumbersTime.includes(NaN)) {
+        alert('Неверный формат данных')
+        return
       }
-    }, 1000);
+      this.buildTimer()
+      const divDays = document.getElementById("timer-days")
+      const divHours = document.getElementById("timer-hours")
+      const divMins = document.getElementById("timer-mins")
+      const divSecs = document.getElementById("timer-secs")
+      let endDate = arrNumbersTime[2] * 1000 + arrNumbersTime[1] * 60 * 1000 + arrNumbersTime[0] * 60 * 60 * 1000
+
+      const timer = setInterval(() => {
+        if (endDate >= 0) {
+          this.flagTimerWork = true
+          let days = Math.floor(endDate / (1000 * 60 * 60 * 24))
+          let hours = Math.floor((endDate / (1000 * 60 * 60)) % 24)
+          let mins = Math.floor((endDate / 1000 / 60) % 60)
+          let secs = Math.floor((endDate / 1000) % 60)
+
+          divDays.innerHTML = days > 0 ? (days+'day(s):') : ''
+          divHours.innerHTML = ("0"+hours+'hour(s):').slice(-10)
+          divMins.innerHTML = ("0"+mins+'min(s):').slice(-9)
+          divSecs.innerHTML = ("0"+secs+'sec(s)').slice(-8)
+
+          endDate = endDate - 1000
+        } else {
+          alert('Таймер закончил свою работу')
+          this.destroyElem('timer-container')
+          clearInterval(timer)
+          this.flagTimerWork = false
+        }
+      }, 1000);
+    }
+  }
+
+  buildTimer() {
+    const container = document.createElement('div')
+    const days = document.createElement('span')
+    const hours = document.createElement('span')
+    const mins = document.createElement('span')
+    const secs = document.createElement('span')
+    const timer = document.createDocumentFragment()
+    container.id = 'timer-container'
+    days.id = 'timer-days'
+    hours.id = 'timer-hours'
+    mins.id = 'timer-mins'
+    secs.id = 'timer-secs'
+    container.append(days, hours, mins, secs)
+    timer.append(container)
+    document.body.append(timer)
+  }
+
+  destroyElem (id) {
+    let deleteElem = document.getElementById(`${id}`);
+    deleteElem.parentNode.removeChild(deleteElem);
   }
 }
