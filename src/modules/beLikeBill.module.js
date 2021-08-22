58a73cab68c8532e39476fbde9d4582941fcab12
Chingis <cbleadr@gmail.com>
2021-08-22 20:15:56 +0300

    created new module beLikeBill

---
+++ src/modules/beLikeBill.module.js
@@ -0,0 +1,29 @@
+import { Module } from "../core/module";
+import * as utils from "../utils";
+
+export default class BeLikeBillModule extends Module {
+    constructor(type, text) {
+        super(type, text);
+    }
+
+    trigger() {
+        const area = utils.getArea();
+        const image = document.createElement('img');
+        image.src = "https://belikebill.ga/billgen-API.php?default=1&name=Bill&sex=m";
+        area.append(image);
+
+
+        let timeOutId;
+
+        image.addEventListener("mouseover", () => {
+            clearTimeout(timeOutId);
+        });
+
+        image.addEventListener("mouseleave", () => {
+            timeOutId = setTimeout(() => {
+                image.remove();
+                window.location.reload(false);
+            }, 2500);
+        });
+    }
+}
