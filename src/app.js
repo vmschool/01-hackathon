a030d07dedcc6c99acef5994ae6c881042f6f068
Iana <Omelchenko@work.co>
2021-08-22 15:15:57 +0300

    random sound

--- src/app.js
+++ src/app.js
@@ -2,12 +2,13 @@ import "./styles.css";
 import { ContextMenu } from "./menu";
 import { counterClick } from "./modules/clicks.module";
 import { counterClickvvv } from "./modules/shape.module";
+import { playRandomSound } from "./modules/randomSound.module";
 
 const menu = new ContextMenu();
 
 // console.log(menu);
 export default menu;
-const menuList = [counterClick, counterClickvvv];
+const menuList = [counterClick, counterClickvvv, playRandomSound];
 // hi.render();
 menuList.forEach((el) => el.addItemInMenuList());
 menu.render();
