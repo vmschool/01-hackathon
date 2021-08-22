ddb9acc84d2f8450d2952c376eaae0077e4023b5
Osmanov <sinaverosmanovfrontend@gmail.com>
2021-08-22 03:00:00 +0300

    bg fix and create fix

--- src/modules/createMusic.module.js
+++ src/modules/createMusic.module.js
@@ -1,91 +1,154 @@
 import { Module } from "../core/module";
-import { random, createEl } from "../utils";
+import { createEl } from "../utils";
 
 export class CreateMusicModule extends Module {
   constructor(type, text) {
     super(type, text);
   }
 
-  trigger() {
-    const keyZ = createEl("div", "z", ["z-key", "sound", "key-shadow"], "z");
-    const keyX = createEl("div", "x", ["x-key", "sound", "key-shadow"], "x");
-    const keyC = createEl("div", "c", ["c-key", "sound", "key-shadow"], "c");
-    const keyV = createEl("div", "v", ["v", "sound", "key-shadow"], "v");
-    const keyB = createEl("div", "b", ["b-key", "sound", "key-shadow"], "b");
-    const keyN = createEl("div", "n", ["n-key", "sound", "key-shadow"], "n");
+  #addSoundToKey(htmlTag, src) {
+    const tag = htmlTag;
+    const audio = createEl("audio", "", [], "");
+    audio.setAttribute("src", src);
+    tag.append(audio);
+    return tag;
+  }
+
+  #createKeys() {
+    const sounds = createEl("div", "", ["sounds"]);
+
+    let keyZ = createEl("div", "z", ["z-key", "sound", "key-shadow"], "z");
+    keyZ = this.#addSoundToKey(
+      keyZ,
+      "http://cd.textfiles.com/maxsounds/WAV/EFEITOS/EXAMPLE.WAV"
+    );
+
+    let keyX = createEl("div", "x", ["x-key", "sound", "key-shadow"], "x");
+    keyX = this.#addSoundToKey(
+      keyX,
+      "http://www.denhaku.com/r_box/tr707/sd1.wav"
+    );
+
+    let keyC = createEl("div", "c", ["c-key", "sound", "key-shadow"], "c");
+    keyC = this.#addSoundToKey(
+      keyC,
+      "http://bigsamples.free.fr/d_kit/hithat/909touch.wav"
+    );
+
+    let keyV = createEl("div", "v", ["v-key", "sound", "key-shadow"], "v");
+    keyV = this.#addSoundToKey(keyV, "http://4umi.com/web/sound/beat.wav");
+
+    let keyB = createEl("div", "b", ["b-key", "sound", "key-shadow"], "b");
+    keyB = this.#addSoundToKey(
+      keyB,
+      "http://www.utc.fr/si28/ProjetsUpload/P2006_si28p004/flash_puzzle/sons/rush/shaker.wav"
+    );
+
+    let keyN = createEl("div", "n", ["n-key", "sound", "key-shadow"], "n");
+    keyN = this.#addSoundToKey(
+      keyN,
+      "http://cd.textfiles.com/maxsounds/WAV/EFEITOS/OH25.WAV"
+    );
+
+    let keyM = createEl(
+      "div",
+      'background tap "m"',
+      ["background", "sound", "key-shadow"],
+      "m"
+    );
+
+    keyM = this.#addSoundToKey(
+      keyM,
+      "http://www.easymusic.stakemann.de/Demo/SoulAlert.mp3"
+    );
+
     const exit = createEl(
       "button",
       "exit",
       ["exit", "sound", "key-shadow"],
       "exit"
     );
+
     exit.addEventListener("click", () => {
       document.querySelector(".sounds").remove();
     });
-    const keyM = createEl(
-      "div",
-      "background",
-      ["background", "sound", "key-shadow"],
-      "m"
-    );
-    const sounds = createEl("div", "", ["sounds", "hide"]);
+
     sounds.append(keyZ, keyX, keyC, keyV, keyB, keyN, keyM, exit);
+    return sounds;
+  }
 
-    document.body.append(sounds);
-
+  #showPanel() {
     document.body.addEventListener("keypress", (event) => {
       const sounds = document.querySelectorAll(".sound");
       sounds.forEach((sound) => sound.classList.add("key-shadow"));
-      switch (event.key) {
-        case "z":
-          const noteDo = document.querySelector("[data='z']");
-          noteDo.classList.remove("key-shadow");
-          new Audio(
-            "http://cd.textfiles.com/maxsounds/WAV/EFEITOS/EXAMPLE.WAV"
-          ).play();
-          break;
-        case "x":
-          const noteRe = document.querySelector("[data='x']");
-          noteRe.classList.remove("key-shadow");
-          new Audio("http://www.denhaku.com/r_box/tr707/sd1.wav").play();
-          break;
-        case "c":
-          const noteMi = document.querySelector("[data='c']");
-          noteMi.classList.remove("key-shadow");
-          new Audio(
-            "http://bigsamples.free.fr/d_kit/hithat/909touch.wav"
-          ).play();
-          break;
-        case "v":
-          const noteFa = document.querySelector("[data='v']");
-          noteFa.classList.remove("key-shadow");
-          new Audio("http://4umi.com/web/sound/beat.wav").play();
-          break;
-        case "b":
-          const noteSo = document.querySelector("[data='b']");
-          noteSo.classList.remove("key-shadow");
-          new Audio(
-            "http://www.utc.fr/si28/ProjetsUpload/P2006_si28p004/flash_puzzle/sons/rush/shaker.wav"
-          ).play();
-          break;
-        case "n":
-          const noteLa = document.querySelector("[data='n']");
-          noteLa.classList.remove("key-shadow");
-          new Audio(
-            "http://cd.textfiles.com/maxsounds/WAV/EFEITOS/OH25.WAV"
-          ).play();
-          break;
-        case "m":
-          const noteTi = document.querySelector("[data='m']");
-          noteTi.classList.remove("key-shadow");
-          new Audio(
-            "http://www.easymusic.stakemann.de/Demo/SoulAlert.mp3"
-          ).play();
-          break;
+      if (document.querySelector(".showMusicKeys")) {
+        switch (event.key) {
+          case "z":
+          case "я":
+            const note1 = document.querySelector("[data='z']");
+            note1.classList.remove("key-shadow");
+            const audio1 = note1.querySelector("audio");
+            audio1.play();
+            break;
+          case "x":
+          case "ч":
+            const note2 = document.querySelector("[data='x']");
+            note2.classList.remove("key-shadow");
+            const audio2 = note2.querySelector("audio");
+            audio2.play();
+            break;
+          case "c":
+          case "с":
+            const note3 = document.querySelector("[data='c']");
+            note3.classList.remove("key-shadow");
+            const audio3 = note3.querySelector("audio");
+            audio3.play();
+            break;
+          case "v":
+          case "м":
+            const note4 = document.querySelector("[data='v']");
+            note4.classList.remove("key-shadow");
+            const audio4 = note4.querySelector("audio");
+            audio4.play();
+            break;
+          case "b":
+          case "и":
+            const note5 = document.querySelector("[data='b']");
+            note5.classList.remove("key-shadow");
+            const audio5 = note5.querySelector("audio");
+            audio5.play();
+            break;
+          case "n":
+          case "т":
+            const noteL6 = document.querySelector("[data='n']");
+            noteL6.classList.remove("key-shadow");
+            const audio6 = noteL6.querySelector("audio");
+            audio6.play();
+            break;
+          case "m":
+          case "ь":
+            const note7 = document.querySelector("[data='m']");
+            note7.classList.remove("key-shadow");
+            const audio7 = note7.querySelector("audio");
+            audio7.play();
+            break;
 
-        default:
-          break;
+          default:
+            break;
+        }
       }
     });
+  }
+
+  trigger() {
+    if (!document.querySelector(".showMusicKeys")) {
+      const soundsHTML = this.#createKeys();
+
+      if (soundsHTML) {
+        document.body.append(soundsHTML);
+        soundsHTML.classList.add("showMusicKeys");
+        this.#showPanel();
+      }
+    }
   }
 }
