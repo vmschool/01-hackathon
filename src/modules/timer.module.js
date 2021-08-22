c6c47e25a5b4e65bf48a4ace12f1203eba2b6830
Liudmylla <liudmyla12@yahoo.fr>
2021-08-22 10:49:47 +0200

    Sweetalert library to have a nice input instead of prompt

--- src/modules/timer.module.js
+++ src/modules/timer.module.js
@@ -1,36 +1,40 @@
 import { Module } from "../core/module";
+import swal from "sweetalert";
 
 export class TimerModule extends Module {
   trigger() {
-    let time = Number(prompt("Put your time in secundes"));
-    if (!time || time > 60) {
-      alert("Your time is wrong");
-    } else {
-      const timeEl = document.createElement("div");
-      const body = document.querySelector("body");
-      timeEl.className = "timeEl";
-      body.append(timeEl);
+    swal("Put your time in seconds here:", {
+      content: "input",
+    }).then((value) => {
+      if (!value || value > 60) {
+        alert("Your time is wrong");
+      } else {
+        const timeEl = document.createElement("div");
+        const body = document.querySelector("body");
+        timeEl.className = "timeEl";
+        body.append(timeEl);
 
-      setInterval(decreaseTime, 1000);
+        setInterval(decreaseTime, 1000);
 
-      function decreaseTime() {
-        if (time === 0) {
-          finishTimer();
-        } else {
-          let current = --time;
-          if (current < 10) {
-            current = `0${current}`;
+        function decreaseTime() {
+          if (value === 0) {
+            finishTimer();
+          } else {
+            let current = --value;
+            if (current < 10) {
+              current = `0${current}`;
+            }
+            setTime(current);
           }
-          setTime(current);
+        }
+        function setTime(value) {
+          timeEl.innerHTML = `00:${value}`;
+        }
+
+        function finishTimer() {
+          timeEl.remove();
         }
       }
-      function setTime(value) {
-        timeEl.innerHTML = `00:${value}`;
-      }
-
-      function finishTimer() {
-        timeEl.remove();
-      }
-    }
+    });
   }
 }
