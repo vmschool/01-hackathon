3640a72c988054b8ffaf9410d7932d2a5af77972
Kirill <k@lis.im>
2021-08-22 07:42:32 +0300

    Initial menu and menu setup

--- src/app.js
+++ src/app.js
@@ -1,2 +1,16 @@
+import { ContextMenu } from './menu'
 import './styles.css'
+
+const contextMenu = new ContextMenu("#menu")
+
+document.addEventListener("contextmenu", (event) => {
+	event.preventDefault()
+    contextMenu.open(event);
+}, true);
+
+document.addEventListener("click", (event) => {
+	event.preventDefault()
+    contextMenu.close(event);
+}, true);
+
 
--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,33 @@
 import {Menu} from './core/menu'
 
 export class ContextMenu extends Menu {
+    
+    constructor( type, text ){
+        super( type, text )
+    }
 
+    add( module ) {
+		this.el.insertAdjacentHTML('beforeend', module.toHTML());
+
+        const menuItems = document.querySelectorAll('.menu-item') 
+        const menuItem = menuItems[menuItems.length - 1]
+
+        menuItem.addEventListener('click', () => {
+            module.trigger();
+		})
+	}
+
+    open( event ){
+        event.preventDefault();
+
+        const el = document.querySelector('#menu')
+		el.style.top = `${event.clientY}px`;
+		el.style.left = `${event.clientX}px`;
+		el.classList.add("open");
+    }
+
+    close(){
+        this.el.classList.remove("open")
+    }
+      
 }
