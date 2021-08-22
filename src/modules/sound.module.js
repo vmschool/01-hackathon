0b3313679b6a5e21a2b8473ec0cc35614dd76e3c
Sergii Sukhoi <mayulka@Sergiis-MacBook-Pro.local>
2021-08-22 15:19:24 +0300

    fix container

--- src/modules/sound.module.js
+++ src/modules/sound.module.js
@@ -7,7 +7,7 @@ export class SoundModule extends Module {
     #container;
     constructor(){
         super('random_sound', 'Воспроизвести звук');
-        this.#container = document.querySelector('body');
+        this.#container = document.querySelector('.container');
         this.#mp3 = ['https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3','http://www.hochmuth.com/mp3/Bloch_Prayer.mp3', 'http://www.hochmuth.com/mp3/Vivaldi_Sonata_eminor_.mp3'];
         this.#audio = new Audio(this.#mp3[random(0,2)]);
     }
