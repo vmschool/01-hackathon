71b66ffb3d40797f5aa86819e9a5342f241a8a68
Maxim <maxtrekusovsite@gmail.com>
2021-11-26 23:50:33 +0300

    Implemented click module

--- src/app.js
+++ src/app.js
@@ -1,2 +1,16 @@
 import './styles.css'
 
+/*
+//в продакшене можно удалить этот тест кликов
+
+import { ClicksModule } from './modules/clicks.module'
+
+const body = document.querySelector('body')
+const click = new ClicksModule('click', 'счётчки кликов')
+const a = document.createElement('a')
+a.textContent = 'Запустить клики'
+body.append(a)
+a.addEventListener('click', () => {
+  click.trigger()
+})
+*/
--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -1,5 +1,54 @@
-import {Module} from '../core/module'
+import { Module } from '../core/module'
 
 export class ClicksModule extends Module {
+  time //кол-во секунд, отведённых для подсчёта
+  clicks //количество нажатий
+  timer //таймер обратного отсчёта
+  body //вся область страницы
+  running //запущен ли сейчас слушатель нажатий
+  listener //функция-обработчик событий
 
+  constructor(type, text) {
+    super(type, text)
+    this.time = 3 //количество отведённых секунд
+    this.clicks = 0
+    this.body = document.querySelector('body')
+    this.running = false
+
+    this.listener = () => {
+      if (this.running) {
+        this.clicks++
+      }
+    }
+  }
+  start() {
+    this.clicks = 0 //обнуляем счётчик
+    this.running = true
+    let curretntTime = this.time //текущее количество секунд
+    this.body.addEventListener('click', this.listener) //запускаем слушатель
+    //запуск обратного отсчёта
+    this.timer = setInterval(() => {
+      if (curretntTime === 0) {
+        this.finish()
+      } else {
+        curretntTime-- //после 1 секунды уменьшаем оставшееся время на 1
+      }
+    }, 1000)
+  }
+
+  finish() {
+    clearInterval(this.timer) //останавливаем таймер
+    this.running = false //остановка подсчётов
+    this.body.removeEventListener('click', this.listener) //удаление обработчика
+    this.toHTML() //вывод сообщения
+  }
+
+  trigger() {
+    this.start() //запуск посчёта кликов
+  }
+
+  toHTML() {
+    //вывод сообщения пользователю
+    alert(`За ${this.time} секунды было сделано ${this.clicks} кликов`)
+  }
 }
