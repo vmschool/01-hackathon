d67c50720a1fb4d5d1b77d574ffa97aa684b6b63
Leonid Lichkun <lionproklon@gmail.com>
2021-11-26 22:44:31 +0300

    Rename branch, added making of Playlist

--- src/modules/random.sound.module.js
+++ src/modules/random.sound.module.js
@@ -1,5 +1,17 @@
 import { Module } from '../core/module'
 
 export class RandomSoundModule extends Module {
+   #makePlaylist
+   #audio
+   constructor() {
 
+   }
+   #makePlaylist(sounds, path = './') {
+      this.#audio = document.createElement('audio')
+      for (let i = 0; i < sounds.length; i++) {
+         this.#audio.id = `sound-${i + 1}`
+         this.#audio.src = `${path}${sounds[i]}`
+         document.body.append(this.#audio)
+      }
+   }
 }
