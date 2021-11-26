e58d5755320f98d5508567bc3b889a1a46fa6faa
Lexxby <iplexx@bk.ru>
2021-11-26 17:19:51 +0300

    add logic to play audio

--- src/modules/audio.module.js
+++ src/modules/audio.module.js
@@ -1,5 +1,18 @@
 import { Module } from '../core/module';
 
 export class AudioModule extends Module {
-  trigger() {}
+  trigger() {
+    //должно отрабатывать надо проверить
+    const urlAudio = 'https://zvukogram.com/mp3/cats/1064/pikachu-malyish.mp3';
+    const audioElement = document.createElement('audio');
+    audioElement.innerHTML = '<source src="' + urlAudio + '" type="audio/mpeg" />';
+    audioElement.play();
+
+    // document.addEventListener('click', function () {
+    //   const audioElement = document.createElement('audio');
+    //   audioElement.innerHTML = '<source src="' + urlAudio + '" type="audio/mpeg" />';
+
+    //   audioElement.play();
+    // });
+  }
 }
