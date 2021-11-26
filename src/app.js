08a99531f2f3473944251b64565bb8829ba320f1
Vladimir Muratov <vladimirmuratov@mail.ru>
2021-11-26 19:22:49 +0300

    create ContextMenu

--- src/app.js
+++ src/app.js
@@ -1,2 +1,9 @@
-import './styles.css'
+import './styles.css';
+import {ContextMenu} from "./menu";
 
+const body = document.querySelector('body')
+body.addEventListener('contextmenu', (e) => {
+    e.preventDefault()
+    const contextMenu = new ContextMenu('#menu')
+    contextMenu.open(e.clientX, e.clientY)
+})
--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,59 @@
 import {Menu} from './core/menu'
+import {BackgroundModule} from "./modules/background.module";
+import {ClicksModule} from "./modules/clicks.module";
+import {ShapeModule} from "./modules/shape.module";
 
 export class ContextMenu extends Menu {
+    constructor(selector) {
+        super(selector);
+    }
 
+    open(x, y) {
+        this.el.style.left = x + 'px'
+        this.el.style.top = y + 'px'
+        this.add(modules)
+        this.el.classList.add('open')
+    }
+
+    close() {
+        this.el.classList.remove('open')
+        this.el.innerHTML = ''
+    }
+
+    add(modules) {
+        if (!modules.length) { // если массив элементов пустой - выходим
+            return
+        }
+        if (!this.el.childNodes.length) { // проходим по массиву и помещаем каждый элемент в ul
+            modules.forEach(module => {
+                this.el.innerHTML += module.toHTML()
+            })
+        }
+        const modulesArr = this.el.querySelectorAll('.menu-item') // находим все li
+        modulesArr.forEach(module => {
+            switch (module.dataset.type) { // на каждый элемент li вешаем слушатель клика в зависимовсти от data-type
+                case 'background':
+                    module.addEventListener('click', () => {
+                        console.log('background')
+                    })
+                    break
+                case 'click':
+                    module.addEventListener('click', () => {
+                        console.log('click')
+                    })
+                    break
+                case 'shape':
+                    module.addEventListener('click', () => {
+                        console.log('shape')
+                    })
+                    break
+            }
+        })
+    }
 }
+
+const modules = [
+    new BackgroundModule('background', 'Поменять цвет'),
+    new ClicksModule('click', 'Считать клики (за 3 секунды)'),
+    new ShapeModule('shape', 'Создать фигуру')
+]
