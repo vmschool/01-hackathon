f2301e6e98b0316853ed9bd9479b95e5e27d4e76
Olga Melnikova <omelnikova@accesssoftek.com>
2021-11-26 19:03:11 +0400

    basic functionality for message appearing and disappearing added

--- src/modules/message.module.js
+++ src/modules/message.module.js
@@ -2,4 +2,49 @@ import { Module } from '../core/module'
 
 export class MessageModule extends Module {
 
+    #message
+    #messageBlock
+    constructor(message, type, text) {
+        super(type, text);
+        this.#message = message;
+
+    }
+    trigger() {
+        try {
+
+            window.addEventListener('click', () => {
+                this.#messageBlock = document.createElement('div');
+                this.#messageBlock.className = 'message';
+                this.#messageBlock.textContent = 'text content';
+                const body = document.querySelector('body');
+                body.append(this.#messageBlock);
+                console.log('click is made');
+            });
+
+
+            const observer = new MutationObserver((body, obs) => {
+                const hello = document.querySelector('.message');
+                if (hello) {
+                    console.log(this.#messageBlock);
+                    setTimeout(() => { hello.remove() }, 2000);
+                    return;
+                }
+
+            });
+
+            observer.observe(document, {
+                childList: true,
+                subtree: true
+            });
+            MutationObserver.disconnect();
+
+
+
+
+            //throw new Error(`Trigger method should be implemented in module "${this.type}"`);
+        } catch (e) {
+            console.log(e.name + ': ' + e.message);
+        }
+    }
+
 }
