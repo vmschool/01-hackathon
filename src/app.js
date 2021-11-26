e6bc2192220caa6e5e7dd1b0da9f43c42b165939
SBilousko <s.bilousko@ukr.net>
2021-11-26 23:22:25 +0200

    Initial commit

--- src/app.js
+++ src/app.js
@@ -1,2 +1,10 @@
-import './styles.css'
+import './styles.css';
+import { ContextMenu } from './menu';
 
+const menu = new ContextMenu();
+document.body.addEventListener('contextmenu', (event) => {
+    event.preventDefault();
+    console.log(event.target);
+    menu.click()
+    menu.open()
+})
--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,10 @@
 import {Menu} from './core/menu'
 
 export class ContextMenu extends Menu {
-
+    click() {
+        console.log('Context menu');
+    }
+    open() {
+        console.log('Open Menu')
+    }
 }
