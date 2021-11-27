ec83920d3185aa2fd7eb4cd6134cd75a545ab032
pavel.mikhailov <pamix1985@gmail.com>
2021-11-28 01:13:51 +0700

    create context menu

--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,57 @@
-import {Menu} from './core/menu'
+import { Menu } from './core/menu';
+import { Module } from './core/module';
 
 export class ContextMenu extends Menu {
+  static CLASS_NAME = 'open';
 
+  constructor(selector, options = {}) {
+    super(selector);
+
+    this.modules = options.modules;
+  }
+
+  open() {
+    this.el.classList.add(ContextMenu.CLASS_NAME);
+  }
+
+  close() {
+    this.el.classList.remove(ContextMenu.CLASS_NAME);
+  }
+
+  add(module) {
+    this.el.insertAdjacentHTML('beforeend', module);
+  }
+
+  fillMenuElement() {
+    this.modules = this.modules.map(Module => new Module);
+
+    this.modules.forEach(moduleInstance => {
+      const moduleElement = moduleInstance.toHTML();
+      
+      this.add(moduleElement);
+    });
+  }
+
+  start() {
+    this.fillMenuElement();
+
+    this.el.addEventListener('click', (event) => {
+      const {target} = event;
+
+      if (target?.dataset.name === Module.DATA_NAME) {
+        const currentType = target.dataset.type;
+        const clickedModuleInstance = this.modules.filter(moduleInstance => moduleInstance.type === currentType)[0];
+  
+        clickedModuleInstance.trigger();
+      }
+    });
+
+    document.addEventListener('contextmenu', (event) => {
+      event.preventDefault();
+
+      this.el.classList.contains(ContextMenu.CLASS_NAME)
+          ? ''
+          : this.open();
+    });
+  }
 }
