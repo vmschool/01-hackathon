19ae3db70d8b3d1fb987e735bd4949b62492c72a
Dan Tkachuk <daniil.tkachuk@gmail.com>
2021-11-27 21:10:05 +0300

    Removed another event listener bug

--- src/app.js
+++ src/app.js
@@ -12,23 +12,25 @@ const modules = [new SoundModule('sound', 'Рандомный звук')];
 
 menu.add(modules);
 
-document.body.addEventListener('contextmenu', (e) => {
-  e.preventDefault();
-  const mouseX = e.pageX;
-  const mouseY = e.pageY;
+function triggerModule(e) {
+  const moduleType = e.target.dataset.type;
+  modules.forEach(item => {
+    if (item.type === moduleType) {
+      item.trigger(e);
+      menu.el.removeEventListener('click', triggerModule);
+      menu.close();
+    }
+  });
+}
 
+function showContextMenu(event) {
+  event.preventDefault();
+  const mouseX = event.pageX;
+  const mouseY = event.pageY;
   menu.open(mouseX, mouseY);
+  menu.el.addEventListener('click', triggerModule);
+  document.body.removeEventListener('click', triggerModule);
+}
 
-  function eventHandler(e) {
-    const moduleType = e.target.dataset.type;
-    modules.forEach(item => {
-      if (item.type === moduleType) {
-        item.trigger(e);
-        menu.el.removeEventListener('click', eventHandler);
-        menu.close();
-      }
-    });
-  }
+document.body.addEventListener('contextmenu', showContextMenu);
 
-  menu.el.addEventListener('click', eventHandler);
-});
