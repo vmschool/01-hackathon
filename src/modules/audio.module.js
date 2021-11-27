da29a7905ac089129fae6d7c96d87853dcebbd27
Lexxby <iplexx@bk.ru>
2021-11-27 14:10:18 +0300

    add constructor

--- src/modules/audio.module.js
+++ src/modules/audio.module.js
@@ -1,18 +1,25 @@
 import { Module } from '../core/module';
+import { random } from '../utils';
 
 export class AudioModule extends Module {
+  constructor(type, text) {
+    super(type, text);
+  }
+
   trigger() {
     const playList = [
       'https://zvukogram.com/mp3/p2/1632/mech-klinok-7-32516.mp3',
       'https://zvukogram.com/mp3/cats/1064/pikachu-malyish.mp3',
       'https://zvukogram.com/mp3/cats/1010/drobovik2.mp3',
+      'https://zvukogram.com/mp3/cats/2722/knopka-klik-aktivnyii-blizkii-chetkii.mp3',
+      'https://zvukogram.com/mp3/cats/1386/gonochnyiy-avtomobil-na-beshennoy-skorosti-skryilsya-za-povorotom.mp3',
     ];
-    const url = playList[0];
-    const audio = new Audio(url);
+    const randNumber = random(0, playList.length);
+    const audio = new Audio(playList[randNumber]);
     document.body.append(audio);
 
     setTimeout(() => {
       audio.play();
-    }, 100);
+    }, 1000);
   }
 }
