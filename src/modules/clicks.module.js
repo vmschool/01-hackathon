8312479ee897da4f1acf6149b8c9fabf3e34d3a4
Aller-Forst <gominzyplay@gmail.com>
2021-11-27 17:08:37 +0300

    Constructor and necessary function added.

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -1,5 +1,28 @@
 import {Module} from '../core/module';
 
 export class ClicksModule extends Module {
+    #time
+    #clickCounts
 
+    static initialParams = {
+        type: 'clicks-analytics',
+        time: 3,
+        text: `Считать клики за определенное время`,   
+    }
+    
+    constructor() {     
+        super(ClicksModule.initialParams.type, ClicksModule.initialParams.text);
+        this.#timeRequest();
+         this.#clickCounts = {
+             singleClick: 0,
+             doubleClick: 0,
+             wheelClick: 0,
+         }
+    }
+
+
+    #timeRequest() {
+        const time = Number(prompt('Введите время для подсчета кликов:').trim());
+        this.#time = time || ClicksModule.initialParams.time;
+    }  
 }
