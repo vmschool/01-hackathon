8af2388a6651d579fa8a4a887e7f2be91ac1f192
slavaOlshanechky <ekaterina.olshanetskaya@gmail.com>
2021-11-28 00:47:34 +0200

    Added:setColor(), getRandomColor()

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,19 +1,30 @@
 import {Module} from '../core/module'
 import {random} from '../utils'
 
+const colors = ['#7CAD44FF', '#AD7544FF', '#c922e6', '#22dfe6', '#C922E6FF']
+
 export class BackgroundModule extends Module {
     constructor() {
-        super('BackgroundModule', 'BackgroundModule');
+        super('BackgroundModule', 'Background switcher');
+        this.body = document.querySelector('body')
+
     }
 
     trigger() {
-        super.trigger();
-        const html = document.querySelector('html')
-        html.style.background = 'red'
+        this.setColor(this.body)
     }
 
-    colorSwitcher() {
-        random()
+    setColor(element) {
+        const color = this.getRandomColor()
+        element.style.backgroundColor = color
+       element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+    }
+     getRandomColor() {
+         let randomNumber = random(1,colors.length)
+
+         const index = Math.floor(randomNumber)
+         console.log(index)
+        return colors[index]
     }
 
 }
