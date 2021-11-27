66f62882348765705139b627efb4b6ff14b7047d
Aller-Forst <gominzyplay@gmail.com>
2021-11-27 17:09:13 +0300

    Result function added.

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -20,6 +20,24 @@ export class ClicksModule extends Module {
          }
     }
 
+    #finish() {
+        const body = document.querySelector('body');
+        const divMain = document.createElement('div');
+        divMain.className = 'clicksAnalytics';
+        const divClicksResult = document.createElement('div');
+        divClicksResult.className = 'clicksAnalytics-div';
+        const singleClicksResult = document.createElement('p');
+        singleClicksResult.textContent = `Одинарных кликов было: ${this.#clickCounts.singleClick}`;
+        const doubleClicksResult = document.createElement('p');
+        doubleClicksResult.textContent = `Двойных кликов было: ${this.#clickCounts.doubleClick}`;
+        const wheelClicksResult = document.createElement('p');
+        wheelClicksResult.textContent = `Кликов на колесико было: ${this.#clickCounts.wheelClick}`;
+        const allClicksResult = document.createElement('p');
+        allClicksResult.textContent = `Всего кликов было: ${this.#clickCounts.singleClick + this.#clickCounts.doubleClick + this.#clickCounts.wheelClick}`;
+        divClicksResult.append(singleClicksResult, doubleClicksResult, wheelClicksResult, allClicksResult);
+        divMain.append(divClicksResult);
+        body.append(divMain);
+    }
 
     #timeRequest() {
         const time = Number(prompt('Введите время для подсчета кликов:').trim());
