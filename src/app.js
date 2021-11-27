85672f521283efb3e5ae94fe110affc4592764b3
CheeseCheesson <kira7999@gmail.com>
2021-11-27 10:27:40 +0300

    add static shape

--- src/app.js
+++ src/app.js
@@ -1,2 +1,6 @@
 import './styles.css'
+import {Menu} from './core/menu'
+import {ShapeModule} from './modules/shape.module'
 
+// const app = new Menu().render()
+const shape = new ShapeModule().render()
--- src/modules/shape.module.js
+++ src/modules/shape.module.js
@@ -1,5 +1,21 @@
 import {Module} from '../core/module'
+import {random} from '../utils'
 
 export class ShapeModule extends Module {
 
+  constructor(){
+    super(ShapeModule, 'ShapeModule')
+    this.width = 100
+    this.height = 100
+    this.color = 'red'
+  }
+
+  render(){
+    const bodyHTML = document.querySelector('body')
+    const figureRootDiv = document.createElement('div')
+    figureRootDiv.style.width = this.width + 'px'
+    figureRootDiv.style.height = this.height + 'px'
+    figureRootDiv.style.backgroundColor = this.color
+    bodyHTML.append(figureRootDiv)
+  }
 }
