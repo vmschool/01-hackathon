2c04344ee1ff1d4da8e5dfd1e02fc8a66ef4a8ed
Leonid Lichkun <lionproklon@gmail.com>
2021-11-27 21:12:34 +0300

    changed trigger and constructor

--- src/modules/sound.module.js
+++ src/modules/sound.module.js
@@ -1,5 +1,5 @@
 import { Module } from '../core/module'
-import * as utils from '../utils'
+import { random } from '../utils'
 import explosion from '../assets/sounds/explosion.mp3'
 import gong from '../assets/sounds/sm-music-gong-1.mp3'
 import drumsticks from '../assets/sounds/drumsticks.mp3'
@@ -8,12 +8,12 @@ import drums from '../assets/sounds/sm-music-drums-2.mp3'
 
 export class RandomSoundModule extends Module {
    #sounds
-   constructor() {
-      super('random-audio', 'Random audio module')
+   constructor(type) {
+      super(type, 'Воспроизвести случайный звук')
       this.#sounds = [explosion, gong, drumsticks, whoosh, drums];
    };
    trigger() {
-      const index = utils.random(1, this.#sounds.length)
+      const index = random(0, this.#sounds.length - 1)
       const audio = new Audio(this.#sounds[index]);
       audio.play()
    }
