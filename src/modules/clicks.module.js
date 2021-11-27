39bc34024814c6e1d6f29c921c0525754dbf4e95
alexion11 <aionov78@yandex.ru>
2021-11-28 00:17:00 +0400

    Click analytics: added separate counters for single and double clicks

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -3,7 +3,8 @@ import { Module } from '../core/module'
 export default class ClicksModule extends Module {
 
 	#clicksTimeoutInSeconds;
-	#numberOfClicks;
+	#numberOfSingleClicks;
+	#numberOfDoubleClicks;
 	#isActive;
 	#timerSingleClick;
 
@@ -16,30 +17,36 @@ export default class ClicksModule extends Module {
 
 	resetState() {
 		this.#clicksTimeoutInSeconds = 3;
-		this.#numberOfClicks = 0;
+		this.#numberOfSingleClicks = 0;
+		this.#numberOfDoubleClicks = 0;
 		this.#isActive = false;
 	}
 
 	setupEventListeners() {
 		document.body.addEventListener('click', (event) => {			
 			this.#isActive = true;
-			if (event.detail === 1) {								
+			if (event.detail === 1) {
 				this.#timerSingleClick = setTimeout(() => {
-					this.clickProcessing();
+					this.clickProcessing(true);
 				}, 200)
 			}	
 		});
 
 		document.body.addEventListener('dblclick', () => {			
 			clearTimeout(this.#timerSingleClick);
-			this.clickProcessing();			
+			this.clickProcessing(false);
 		});
 	}
 
-	clickProcessing() {
-		if(this.#isActive) {
-			this.#numberOfClicks++;
-			console.log('Number of clicks:', this.#numberOfClicks);
+	clickProcessing(isSingleClick) {
+		if(this.#isActive) {			
+			if(isSingleClick) {
+				this.#numberOfSingleClicks++;
+				console.log('Number of single clicks:', this.#numberOfSingleClicks);
+			} else {
+				this.#numberOfDoubleClicks++;
+				console.log('Number of double clicks:', this.#numberOfDoubleClicks);
+			}			
 		}
 	}
 	
@@ -54,6 +61,6 @@ export default class ClicksModule extends Module {
 
 	clickTimeout() {
 		this.#isActive = false;
-		alert(`Кол-во кликов ${this.#numberOfClicks} за ${this.#clicksTimeoutInSeconds} cек.`)
+		alert(`Кол-во одиночных кликов ${this.#numberOfSingleClicks}\nКол-во двойных кликов ${this.#numberOfDoubleClicks}`)
 	}	
 }
