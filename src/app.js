342f9e812d9ff4e4c2a9722f12ab518ddd1757db
eximenz <terpigoreva1992@gmail.com>
2021-11-27 20:11:53 +0300

    added RandomSound Module

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import './styles.css'
 import {ContextMenu} from './menu';
 import { BackgroundModule } from './modules/background.module';
+import { RandomSound } from './modules/randomSound.module';
+
 
 const contextMenu = new ContextMenu('.menu');
 
@@ -8,8 +10,16 @@ document.body.addEventListener('contextmenu', event => {
 	contextMenu.open(event);
 })
 
+document.body.addEventListener("click", (event) => {
+  contextMenu.close(event);
+});
+
 const backgroundModule = new BackgroundModule('background', 'To generate random background');
 contextMenu.add(backgroundModule);
+
+const randomSound = new RandomSound('randomSound', 'To generate random sound');
+contextMenu.add(randomSound);
+
 
 
 
--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -2,9 +2,11 @@ import { Module } from '../core/module'
 import { random } from '../utils';
 
 export class BackgroundModule extends Module {
+
     constructor(type, text) {
-        super(type, text)
+      super(type, text);
     }
+
     trigger() {
       document.body.style.background = `RGB(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)})`
     }
