3bb6b0513f9f89a4f50a79520e8b902f1ff56d67
glava456 <glava456@gmail.com>
2021-11-27 22:55:00 +0200

    add reset and close buttons

--- src/modules/timer.module.js
+++ src/modules/timer.module.js
@@ -2,31 +2,33 @@ import {Module} from '../core/module';
 
 export class Timer extends Module {
   trigger() {
-  const timer = document.createElement('div');
-  timer.classList = 'timer'
-  timer.innerHTML = `      
-  <div class="timer__background">
-    <div class="timer__display">
-      <div class="display minute"></div>
-      <span class="display colon">:</span>
-      <div class="display seconds"></div>
-    </div>
-  </div>
-  <div class="timer-input">
-    <div class="input__container">
-      <label for="inp-minute">Minutes</label>
-      <input type="text" id="inp-minute" placeholder="0" />
-    </div>
-    <div class="input__container">
-      <label for="inp-seconds">Seconds</label>
-      <input type="text" id="inp-seconds" placeholder="0" />
-    </div>
-  </div>
-  <div class="btn-group">
-    <button class="btn start">start</button>
-  </div>
-  <audio class="audio" src="https://soundbible.com/mp3/service-bell_daniel_simion.mp3" type="audio/mp3"></audio>
-`
+    const timer = document.createElement('div');
+    timer.classList = 'timer'
+    timer.innerHTML = `      
+        <div class="timer__background">
+        <div class="timer__close"></div>
+          <div class="timer__display">
+            <div class="display minute"></div>
+            <span class="display colon">:</span>
+            <div class="display seconds"></div>
+          </div>
+        </div>
+        <div class="timer-input">
+          <div class="input__container">
+            <label for="inp-minute">Minutes</label>
+            <input type="text" id="inp-minute" placeholder="0" />
+          </div>
+          <div class="input__container">
+            <label for="inp-seconds">Seconds</label>
+            <input type="text" id="inp-seconds" placeholder="0" />
+          </div>
+        </div>
+        <div class="btn-group">
+          <button class="start">start</button>
+          <button class="reset">reset</button>
+        </div>
+        <audio class="audio" src="https://soundbible.com/mp3/service-bell_daniel_simion.mp3" type="audio/mp3"></audio>
+      `
     document.querySelector('body').prepend(timer)
 
     // getting Display minutes and seconds elements
@@ -41,6 +43,8 @@ export class Timer extends Module {
 
     // getting all the button
     const start = document.querySelector(".start");
+    const reset = document.querySelector(".reset");
+    const timerClose = document.querySelector(".timer__close");
 
     // getting audio
     const music = document.querySelector(".audio");
@@ -56,11 +60,15 @@ export class Timer extends Module {
     const textCorrection = (element, value) => {
       element.innerHTML = value < 10 ? "0" + value : value;
     }
+    timerClose.addEventListener("click", () => {
+      timer.remove()
+    })
 
     start.addEventListener("click", () => {
       totalTime = inpMinutes.value * 60 + inpSeconds.value * 1;
       if (inpMinutes.value != "" || inpSeconds.value != "") {
-        start.setAttribute('disabled', "")
+        
+
         interval = setInterval(() => {
           const minutes = Math.floor(totalTime / 60);
           const seconds = totalTime % 60;
@@ -70,13 +78,20 @@ export class Timer extends Module {
 
           if (totalTime > 0) {
             totalTime--;
+            reset.addEventListener("click", () => {
+              disMinutes.innerHTML = "00";
+              disSeconds.innerHTML = "00";
+              inpMinutes.value = ''
+              inpSeconds.value = ''
+              clearInterval(interval)
+            })
           } else {
             clearInterval(interval)
             timerDisplay.remove()
             start.remove()
             music.play();
             timer.innerHTML = `<p class="end_time">
-            time is over</p>`
+                  time is over</p>`
             setTimeout(() => timer.remove(), 2000)
           }
         }, 1000);
