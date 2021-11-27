3f0ee69610e3a682b910208085311c326762e4d7
Dan Tkachuk <daniil.tkachuk@gmail.com>
2021-11-27 15:26:40 +0300

    Menu finished

--- src/app.js
+++ src/app.js
@@ -1,2 +1,31 @@
 import './styles.css';
+import {
+  ContextMenu
+} from './menu';
+import {
+  SoundModule
+} from './modules/sound.module';
 
+const menu = new ContextMenu('#menu');
+
+const modules = [new SoundModule('sound', 'Случайный звук'), new SoundModule('sound', 'Boom')];
+
+menu.add(modules);
+
+document.body.addEventListener('contextmenu', (e) => {
+  e.preventDefault();
+  const mouseX = e.pageX;
+  const mouseY = e.pageY;
+
+  menu.open(mouseX, mouseY);
+
+
+  menu.el.addEventListener('click', (e) => {
+    const moduleType = e.target.dataset.type;
+    modules.forEach(item => {
+      if (item.type === moduleType) {
+        item.trigger();
+      }
+    });
+  });
+});
