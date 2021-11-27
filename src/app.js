48cdf64edc21a6dec73e0d4e649a2bb5d8de3f84
eximenz <terpigoreva1992@gmail.com>
2021-11-27 17:02:21 +0300

    added contextMenu

--- src/app.js
+++ src/app.js
@@ -1,2 +1,20 @@
 import './styles.css'
+import {ContextMenu} from './menu';
+import { BackgroundModule } from "./modules/background.module";
+
+const contextMenu = new ContextMenu('.menu');
+
+document.body.addEventListener('contextmenu', event => {
+	contextMenu.open(event);
+})
+
+const backgroundModule = new BackgroundModule('background', 'Изменить фон');
+contextMenu.add(backgroundModule);
+
+
+
+
+
+
+
 
--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,25 @@
-import {Menu} from './core/menu'
+import {Menu} from './core/menu';
 
 export class ContextMenu extends Menu {
 
+  constructor(selector) {
+    super(selector);
+  }
+
+  open(event) {
+    event.preventDefault();
+    this.el.classList.add("open");
+  }
+
+  close() {
+    this.el.classList.remove("open");
+  }
+
+  add(module) {
+    this.el.insertAdjacentHTML("beforeend", module.toHTML());
+
+    this.el.addEventListener("click", (event) => {
+      module.type === event.target.dataset.type ? module.trigger() : false;
+    })
+  }
 }
