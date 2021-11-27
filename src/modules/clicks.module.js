e5c8cb97b7adc251ef2d0d8c6ecfc2e24c179cb1
alexion11 <aionov78@yandex.ru>
2021-11-27 17:54:26 +0400

    Implemented click analytics

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -2,12 +2,35 @@ import { Module } from '../core/module'
 
 export default class ClicksModule extends Module {
 
+	#clicksTimeoutMsec = 3000;
+	#numberOfClicks = 0;
+	#isActive = false;
+
 	constructor(text) {
-		super('Clicks', text);
+		super('Clicks', text);		
 	}
 	
 	trigger() {
 		console.log('Clicks triggered');
+		
+		this.#numberOfClicks = 0;
+		this.#isActive = true;
+
+		setTimeout(() => {			
+			this.clickTimeout();
+		},  this.#clicksTimeoutMsec);
+
+		document.body.addEventListener('click', (event) => {			
+			if(this.#isActive) {
+				this.#numberOfClicks++;
+				console.log('Number of clicks:', this.#numberOfClicks);
+			}						
+		});
+	}
+
+	clickTimeout() {
+		alert(`Кол-во кликов ${this.#numberOfClicks} за ${this.#clicksTimeoutMsec} Мсек.`)
+		this.#isActive = false;
 	}
 	
 }
