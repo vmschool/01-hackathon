f795a08abd95b09ba550b4689aa5884a828d3324
alexion11 <aionov78@yandex.ru>
2021-11-27 19:53:48 +0400

    Improved implementation of click analytics

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -2,35 +2,61 @@ import { Module } from '../core/module'
 
 export default class ClicksModule extends Module {
 
-	#clicksTimeoutMsec = 3000;
-	#numberOfClicks = 0;
-	#isActive = false;
+	#clicksTimeoutInSeconds;
+	#numberOfClicks;
+	#isActive;
+	#timerSingleClick;
 
 	constructor(text) {
 		super('Clicks', text);		
+
+		this.#clicksTimeoutInSeconds = 3;
+		this.#numberOfClicks = 0;
+		this.#isActive = false;
+
+		this.setupEventListeners();
+	}
+
+	resetState() {
+		this.#numberOfClicks = 0;
+		this.#isActive = false;
+	}
+
+	setupEventListeners() {
+		document.body.addEventListener('click', (event) => {			
+			if (event.detail === 1) {				
+				this.#timerSingleClick = setTimeout(() => {					
+					this.clickProcessing();
+				}, 200)
+			}	
+		});
+
+		document.body.addEventListener('dblclick', () => {			
+			clearTimeout(this.#timerSingleClick);
+			this.clickProcessing();			
+		});
+	}
+
+	clickProcessing() {
+		if(this.#isActive) {
+			this.#numberOfClicks++;
+			console.log('Number of clicks:', this.#numberOfClicks - 1);
+		}
 	}
 	
 	trigger() {
 		console.log('Clicks triggered');
 		
 		this.#numberOfClicks = 0;
-		this.#isActive = true;
+		this.#isActive = true;		
 
 		setTimeout(() => {			
 			this.clickTimeout();
-		},  this.#clicksTimeoutMsec);
-
-		document.body.addEventListener('click', (event) => {			
-			if(this.#isActive) {
-				this.#numberOfClicks++;
-				console.log('Number of clicks:', this.#numberOfClicks);
-			}						
-		});
-	}
+		},  this.#clicksTimeoutInSeconds * 1000);		
+	}	
 
 	clickTimeout() {
-		alert(`Кол-во кликов ${this.#numberOfClicks} за ${this.#clicksTimeoutMsec} Мсек.`)
 		this.#isActive = false;
-	}
-	
+		alert(`Кол-во кликов ${this.#numberOfClicks - 1} за ${this.#clicksTimeoutInSeconds} cек.`)
+	}	
 }
