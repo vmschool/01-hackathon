6c92d65f158ce87386002aee086653d35d73fc6d
slavaOlshanechky <ekaterina.olshanetskaya@gmail.com>
2021-11-27 23:42:46 +0200

    initial commit

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,5 +1,19 @@
 import {Module} from '../core/module'
+import {random} from '../utils'
 
 export class BackgroundModule extends Module {
+    constructor() {
+        super('BackgroundModule', 'BackgroundModule');
+    }
+
+    trigger() {
+        super.trigger();
+        const html = document.querySelector('html')
+        html.style.background = 'red'
+    }
+
+    colorSwitcher() {
+        random()
+    }
 
 }
