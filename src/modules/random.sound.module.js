d37ac0140003c122f701a0b8e402d96e23dea00a
Leonid Lichkun <lionproklon@gmail.com>
2021-11-27 19:23:43 +0300

    Added path 'assets/sounds' add sounds, added method trigger(), importing all sounds, changed webpack.config and package.json

--- src/modules/random.sound.module.js
+++ src/modules/random.sound.module.js
@@ -1,17 +1,20 @@
 import { Module } from '../core/module'
+import * as utils from '../utils'
+import explosion from '../assets/sounds/explosion.mp3'
+import gong from '../assets/sounds/sm-music-gong-1.mp3'
+import drumsticks from '../assets/sounds/drumsticks.mp3'
+import whoosh from '../assets/sounds/whoosh.mp3'
+import drums from '../assets/sounds/sm-music-drums-2.mp3'
 
 export class RandomSoundModule extends Module {
-   #makePlaylist
-   #audio
+   #sounds
    constructor() {
-
-   }
-   #makePlaylist(sounds, path = './') {
-      this.#audio = document.createElement('audio')
-      for (let i = 0; i < sounds.length; i++) {
-         this.#audio.id = `sound-${i + 1}`
-         this.#audio.src = `${path}${sounds[i]}`
-         document.body.append(this.#audio)
-      }
+      super('random-audio', 'Random audio module')
+      this.#sounds = [explosion, gong, drumsticks, whoosh, drums];
+   };
+   trigger() {
+      const index = utils.random(1, this.#sounds.length)
+      const audio = new Audio(this.#sounds[index]);
+      audio.play()
    }
 }
