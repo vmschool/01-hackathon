8642f80c471b50b15125411a7c93541601ccb829
Urman <subwme@gmail.com>
2021-11-27 17:32:57 +0300

    update custom.module.js, update style.css

--- src/modules/custom.message.module.js
+++ src/modules/custom.message.module.js
@@ -3,42 +3,61 @@ import {random} from '../utils'
 
 export class CustomMessage extends Module {
   constructor() {
+    //container
     this.customMessageContainer = document.createElement('div')
-    this.customMessageContainer.className = 'custom-message-block'
+    this.customMessageContainer.className = 'container-message'
+    //messageBlock
     this.customMessageBlock = document.createElement('div')
     this.customMessageBlock.className = 'custom-message'
+    //closeElement
+    this.closeCustomMessage = document.createElement('button')
+    this.closeCustomMessage.className = 'block__btn'
+    this.closeCustomMessage.innerHTML = '&times;' 
   }
+    //пока не могу понять, как инициадизировать type
+    super()
 
   renderCustomMessage () {
-    const customMessageText = document.createElement('span')
-    customMessageText.className = 'custom-message-text'
-    customMessageText.innerHTML = this.addMessage()
-    this.customMessageBlock.append(customMessageText)
+    //renderHTML
+    this.customMessageBlock.classList.add('show')
+    this.customMessageContainer.style.display = 'block'
+    //message
+    const customMessageText = document.createElement('div')
+    customMessageText.className = 'block__message'
+    //addRandomMessage
+    customMessageText.textContent = this.addMessage()
+    //add HTMLelements
+    this.customMessageBlock.append(this.closeCustomMessage, customMessageText)
     this.customMessageContainer.append(this.customMessageBlock)
+    //Listenner
     this.deleteCustomMessage()
+
     return this.customMessageContainer
   }
 
   addMessage() {
+    //textMessage
     const mockMessages = [
       'Что-то пошло не так..',
       'Добро пожаловать!',
       'Еще увидимся!'
     ]
-    return mockMessages[Utils.random(0,2)]
+    return mockMessages[random(0,2)]
     
   }
 
   trigger() {
-    //this.messages = mockMessages
     this.renderCustomMessage()
   }
 
   deleteCustomMessage() {
-    setTimeout(() => {
-      this.customMessageContainer.remove(this.customMessageBlock)
-    }, 4000)
+    this.closeCustomMessage.addEventListener('click', () => {
+      this.customMessageContainer.style.display = 'none'
+      setTimeout(() => {
+        document.body.remove(this.customMessageContainer)
+      }, 3000)
+    }
+    )
   }
-
 }
 
