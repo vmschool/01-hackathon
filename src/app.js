1821a6ff6d0f9c159f7a486a0118eb2b91871a53
Leonid Lichkun <lionproklon@gmail.com>
2021-11-28 15:17:23 +0300

    Clear app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,2 @@
 import './styles.css'
-import { RandomSoundModule } from './modules/sound.module'
 
-
-const snd = new RandomSoundModule('type')
-
-const btn = document.createElement('button');
-btn.textContent = 'CLick me'
-document.body.append(btn)
-btn.addEventListener('click', () => snd.trigger())
