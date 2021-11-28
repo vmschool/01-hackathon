347eaa731d04ccf96b5e87a47c97283f564a8c75
pavel.mikhailov <pamix1985@gmail.com>
2021-11-28 18:00:25 +0700

    fixed incapsulation

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -4,9 +4,11 @@ import {random} from '../utils'
 export class BackgroundModule extends Module {
   static TYPE = 'BackgroundModule'
   static TEXT = 'Случайный фон'
+  #backgroundColors
+
   constructor() {
     super(BackgroundModule.TYPE, BackgroundModule.TEXT)
-    this.backgroundColors = ['#0d43b8', '#1c9e34',
+    this.#backgroundColors = ['#0d43b8', '#1c9e34',
                              '#0b6880', '#5b00c4',
                              '#8700a8', '#990000',
                              '#00ab6c', '#1a1a1a',
@@ -15,8 +17,8 @@ export class BackgroundModule extends Module {
   }
 
   getRandomColor() {
-    const index = random(0, this.backgroundColors.length - 1)
-    return this.backgroundColors[index]
+    const index = random(0, this.#backgroundColors.length - 1)
+    return this.#backgroundColors[index]
   }
 
   trigger() {
