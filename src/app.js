723deb661cacf6b6585fe7f93bb790b678a097f4
pavel.mikhailov <pamix1985@gmail.com>
2021-11-28 19:47:05 +0700

    import modules

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 import './styles.css';
 import { ContextMenu } from './menu';
 import { BackgroundModule } from './modules/background.module';
+import { AudioModule } from './modules/module-audio';
+import { VideoModule } from './modules/module-video';
+import { ShapeModule } from './modules/shape.module';
+
 
 const menu = new ContextMenu('.menu', {
-  modules: [BackgroundModule],
+  modules: [BackgroundModule, AudioModule, VideoModule, ShapeModule],
 });
 
 menu.start();
