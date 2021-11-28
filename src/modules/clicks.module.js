b5609ba5773c5ef6b6d0cc6b2fc35bec1e2a9ed3
alexion11 <aionov78@yandex.ru>
2021-11-29 00:22:48 +0400

    Clicks analytics: added timer

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -1,32 +1,36 @@
 import { Module } from '../core/module'
-import styles from './clicks.module.css'
+import './clicks.module.css'
 
 export default class ClicksModule extends Module {
 
 	#clicksTimeoutInSeconds;
 	#numberOfSingleClicks;
 	#numberOfDoubleClicks;
+	#timerSingleClick;	
+	#timerValue;
+	#timerDisplayId;
 	#isActive;
-	#timerSingleClick;
 
 	constructor(text) {
 		super('Clicks', text);
 		this.#resetState();
-		//this.#setupEventListeners();
+		this.#setupEventListeners();
 	}
 	
 	trigger() {
-		console.log('Clicks triggered');		
+		console.log('Clicks triggered');
 		this.#resetState();
 		this.#createHtml();
-		this.#setupEventListeners();
-		this.#startTimer();
+		this.#updateAnalyticsInfo();
+		//this.#setupEventListeners();
+		this.#startAnalytics();
 	}
 
 	#resetState() {
-		this.#clicksTimeoutInSeconds = 5;
+		this.#clicksTimeoutInSeconds = 5;		
 		this.#numberOfSingleClicks = 0;
-		this.#numberOfDoubleClicks = 0;
+		this.#numberOfDoubleClicks = 0;		
+		this.#timerValue = this.#clicksTimeoutInSeconds;
 		this.#isActive = false;
 	}
 
@@ -34,6 +38,7 @@ export default class ClicksModule extends Module {
 		if (event.detail === 1) {
 			this.#timerSingleClick = setTimeout(() => {
 				this.#clickProcessing(true);
+				this.#updateAnalyticsInfo();
 			}, 200)
 		}	
 	}
@@ -41,6 +46,7 @@ export default class ClicksModule extends Module {
 	#doubleClickHandler = function() {
 		clearTimeout(this.#timerSingleClick);
 		this.#clickProcessing(false);
+		this.#updateAnalyticsInfo();
 	}
 
 	#setupEventListeners() {
@@ -52,7 +58,7 @@ export default class ClicksModule extends Module {
 	}
 
 	#clickProcessing(isSingleClick) {
-		if(this.#isActive) {			
+		if(this.#isActive) {
 			if(isSingleClick) {
 				this.#numberOfSingleClicks++;
 				console.log('Number of single clicks:', this.#numberOfSingleClicks);
@@ -60,37 +66,68 @@ export default class ClicksModule extends Module {
 				this.#numberOfDoubleClicks++;
 				console.log('Number of double clicks:', this.#numberOfDoubleClicks);
 			}
-			this.#updateAnalyticsOfClicks();
 		}
 	}	
 
-	#startTimer() {		
+	#startAnalytics() {
 		this.#isActive = true;
-		setTimeout(() => {						
+		this.#updateTimer(this.#timerValue);
+		setTimeout(() => {
 			this.#isActive = false;
-		},  this.#clicksTimeoutInSeconds * 1000);		
+		},  this.#clicksTimeoutInSeconds * 1000);
+		clearInterval(this.#timerDisplayId);
+		this.#timerDisplayId = setInterval(this.#displayTimer.bind(this), 1000);
 	}
 
-	#updateAnalyticsOfClicks() {				
+	#displayTimer() {
+		console.log('displayTimer', this.#timerValue);
+		if(this.#timerValue !== 0) {
+			let value = --this.#timerValue;
+			if(value < this.#clicksTimeoutInSeconds) {
+				value = `0${value}`;
+			}
+			this.#updateTimer(value);
+		} else {
+			clearInterval(this.#timerDisplayId);
+		}
+	}
+
+	#updateAnalyticsInfo() {
 		let singleClicks = document.querySelector('#clicks-module-single-counter-number');
 		if(singleClicks) {
-			singleClicks.textContent = `${this.#numberOfSingleClicks}`;
+			singleClicks.textContent = this.#numberOfSingleClicks <= 0 ? 0 : `${this.#numberOfSingleClicks - 1}`;
 		}
 
 		let doubleClicks = document.querySelector('#clicks-module-double-counter-number');
 		if(doubleClicks) {
 			doubleClicks.textContent = `${this.#numberOfDoubleClicks}`;
-		}
-	}	
+		}		
+	}
 
-	#createHtml() {		
-		let root = document.querySelector('.root-clicks-module');
-		if(root) {			
-			document.body.removeChild(root);
+	#updateTimer(value) {
+		let timerElem = document.querySelector('#clicks-module-timer-id');
+		timerElem.textContent = `00:${value}`;
+	}
+
+	#createHtml() {
+		let moduleExists = document.querySelector('.root-clicks-module');
+		if(moduleExists) {
+			return;
 		}
 
-		let div = document.createElement("div");
-		div.className = 'root-clicks-module';
+		let module = document.createElement("div");
+		module.className = 'root-clicks-module';
+
+		let timerBox = document.createElement("div");
+		timerBox.className = 'clicks-module-timer-box';
+
+		let countersBox = document.createElement("div");
+		countersBox.className = 'clicks-module-counters-box';
+
+		let timerElement = document.createElement("div");
+		timerElement.className = 'clicks-module-timer';
+		timerElement.textContent = '00:00';
+		timerElement.id = 'clicks-module-timer-id';		
 		
 		let singleClickBox = document.createElement("div");
 		singleClickBox.className = 'clicks-module-box';
@@ -120,11 +157,14 @@ export default class ClicksModule extends Module {
 		doubleClickBox.appendChild(doubleClickCaption);
 		doubleClickBox.appendChild(doubleClickCounter);
 
-		div.appendChild(singleClickBox);
-		div.appendChild(doubleClickBox);
-		
-		document.body.append(div);
+		timerBox.appendChild(timerElement);
 
-		this.#updateAnalyticsOfClicks();
+		countersBox.appendChild(singleClickBox);
+		countersBox.appendChild(doubleClickBox);
+		
+		module.appendChild(timerBox);
+		module.appendChild(countersBox);
+		
+		document.body.append(module);
 	}
 }
