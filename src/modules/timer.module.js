4028d8e70c62342edb411b7ecaef527b1bcf98a8
MichaelD <enee.kors@mail.ru>
2021-11-28 14:21:23 +0300

    improve timer

--- src/modules/timer.module.js
+++ src/modules/timer.module.js
@@ -1,55 +1,74 @@
-import { Module } from '../core/module'
+import {Module} from '../core/module'
 
 export default class TimerModule extends Module {
-	
-	constructor(text) {
-		super('Timer', text);
-	}
 
-	trigger() {
-		console.log('Timer triggered');
+    constructor(text) {
+        super('Timer', text);
+    }
 
-		const timer = document.createElement('div');
-		timer.className = 'timer_container';
+    trigger() {
+        console.log('Timer triggered');
 
-		const input = document.createElement('input');
-		input.className = 'timer_input';
-		input.name = 'seconds';
-		input.type = 'number';
-		input.max = '60';
-		input.min = '0';
 
-		const button = document.createElement('button');
-		button.className = 'button_start';
-		button.textContent = 'запустить';
+        const timer = document.createElement('div');
+        timer.className = 'timer_container';
 
-		const timeCountdown = document.createElement('div');
-		timeCountdown.className = 'time_countdown';
-		timeCountdown.textContent = '';
+        const button = document.createElement('button');
+        button.className = 'button_start';
+        button.textContent = 'запустить';
 
-		timer.append(input, button);
-		document.body.append(timer);
+        let inputHTML = `
+<input class="timer_input_hours" name="hours" type="number" max="59" min="0">
+<input class="timer_input_min" name="minutes" type="number" max="59" min="0">
+<input class="timer_input_sec" name="seconds" type="number" max="59" min="0">`;
 
-		button.addEventListener('click', (event) => {
-			let timerInput = document.querySelector('.timer_input')
-			let timerSec = Number(timerInput.value)
-			timer.append(timeCountdown)
-			const timerFunc = setInterval(function() {
-				if (timerSec <= 0) {
-					clearInterval(timerFunc)
-					alert("time is over")
-					timer.innerHTML = ''
-				} else {
-					let stringTimeSec = `${Math.trunc(timerSec)}`;
-					const timeCountdown = document.querySelector('.time_countdown');
-					timeCountdown.innerHTML = stringTimeSec;
-					console.log(stringTimeSec)
-					console.log(this.time)
-				}
-				--timerSec;
-			}, 1000)
+        let timeCountdownContainerHTML = `
+<div class="time_countdown_container">
+    <p id="timer">
+        <span id="timer-hours"></span>
+        <span id="timer-mins"></span>
+        <span id="timer-secs"></span>
+    </p>
+</div>`;
 
-		})
-	}
+        timer.innerHTML = inputHTML;
+        timer.append(button);
+        document.body.append(timer);
+
+        button.addEventListener('click', (event) => {
+            let timerInputSec = document.querySelector('.timer_input_sec');
+            let timerInputMin = document.querySelector('.timer_input_min');
+            let timerInputHours = document.querySelector('.timer_input_hours');
+            let timerSec = Number(timerInputSec.value);
+            let timerMin = Number(timerInputMin.value);
+            let timerHours = Number(timerInputHours.value);
+            console.log(timerSec);
+            console.log(timerMin);
+            console.log(timerHours);
+            const endDate = new Date();
+            endDate.setHours((endDate.getHours() + timerHours), (endDate.getMinutes() + timerMin), (endDate.getSeconds() + timerSec+1));
+
+            timer.innerHTML = timeCountdownContainerHTML;
+
+            const timerFunc = setInterval(function () {
+                const currentDate = new Date();
+
+                let deltaTime = endDate.getTime() - currentDate.getTime();
+
+                if (deltaTime >= 0) {
+                    let hours = Math.floor((deltaTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                    let mins = Math.floor((deltaTime % (1000 * 60 * 60)) / (1000 * 60));
+                    let secs = Math.floor((deltaTime % (1000 * 60)) / 1000);
+
+                    document.getElementById('timer-hours').innerHTML = ('0' + hours).slice(-2) + '<span class="label">HR(S)</span>';
+                    document.getElementById('timer-mins').innerHTML = ('0' + mins).slice(-2) + '<span class="label">MIN(S)</span>';
+                    document.getElementById('timer-secs').innerHTML = ('0' + secs).slice(-2) + '<span class="label">SEC(S)</span>';
+                } else {
+                    document.getElementById('timer').innerHTML = 'TIME IS OVER';
+                }
+            }, 1000);
+
+        })
+    }
 
 }
