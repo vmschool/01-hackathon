e298d6e81322909ab65d37d4d805a5ccfe9df7ca
marxt2016 <marxt@yandex.ru>
2021-11-28 15:44:06 +0400

    Module message (#2)
    * initial module-message
    
    * basic functionality for message appearing and disappearing added
    
    * updated styles and random message position
    
    * removed event listener
    
    * clean up
    
    * resolved comments about document.body
    
    * corrected constructor
    
    * changed text to RU
    
    * changet timeout
    
    * corrected comments after teamlead review
    
    * corrected spaces
    
    Co-authored-by: Olga Melnikova <omelnikova@accesssoftek.com>

---
+++ src/modules/message/message.module.js
@@ -0,0 +1,38 @@
+import { Module } from '../../core/module';
+import { random } from '../../utils';
+import './message.css';
+
+export class MessageModule extends Module {
+
+    static #messagesArray = ['Have a nice weekend!', 'Happy Thanksgiving Day!', 'All the leaves are brown)', 'Id be safe and warm'];
+    static TYPE = 'MessageModule';
+    static TEXT = 'Показать сообщение';
+
+    constructor() {
+        super(MessageModule.TYPE, MessageModule.TEXT);
+    }
+
+    trigger() {
+        const messageBlock = this.createMessageElement();
+        document.body.append(messageBlock);
+        const messageTimeout = setTimeout(() => {
+            messageBlock.remove();
+            clearTimeout(messageTimeout);
+        }, 3000);
+    };
+
+    createMessageElement() {
+        const randomIndex = random(0, MessageModule.#messagesArray.length - 1);
+        const messageBlock = document.createElement('div');
+        messageBlock.className = 'message';
+        messageBlock.textContent = MessageModule.#messagesArray[randomIndex];
+        const winWidth = window.innerWidth;
+        const winHeight = window.innerHeight;
+        const randomTop = random(0, winHeight - 100);
+        const randomLeft = random(0, winWidth - 100);
+        messageBlock.style.top = randomTop + 'px';
+        messageBlock.style.left = randomLeft + 'px';
+
+        return messageBlock;
+    };
+};
