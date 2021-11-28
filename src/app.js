e15a6d4fc7fbf77b0709b547f2a40355e30e9187
Yehor Fomichov <argo.yegor@gmail.com>
2021-11-28 08:15:33 +0200

    to initial

--- src/app.js
+++ src/app.js
@@ -1,10 +1,1 @@
 import './styles.css'
-
-import { GameOfLife } from './modules/gol'
-
-const gameOfLife = new GameOfLife()
-
-document.body.append(gameOfLife.createWorld())
-
-const currentGen = gameOfLife.startGame()
-setInterval(() => {gameOfLife.updateWorld()}, 1000)
--- src/constants.js
+++
@@ -1,4 +0,0 @@
-export const golGrit = {
-    rows: 40,
-    cols: 40
-}
