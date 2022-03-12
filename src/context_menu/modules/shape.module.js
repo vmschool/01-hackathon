69f59497c5a9c575ef32f28936c387e3045a9fd8
Notkabene <len1618@yandex.ru>
2022-03-12 18:15:09 +0300

    Статичные стили вынесены в css

--- src/context_menu/modules/shape.module.js
+++ src/context_menu/modules/shape.module.js
@@ -31,26 +31,17 @@ export class ShapeModule extends Module {
         const shapeElement = document.createElement('div')
         const shapeInnerElement = document.createElement('div')
         shapeElement.classList = 'shape'
-        shapeElement.style.opacity = '1'
-        shapeElement.style.transition = 'opacity 0.5s'
         shapeElement.style.width = `${UTILS.random(50, 300)}px`
         shapeElement.style.height = `${UTILS.random(50, 300)}px`
         shapeElement.style.borderRadius = `${UTILS.random(0, 50)}%`
         shapeElement.style.backgroundColor = `RGB(${UTILS.random(0, 255)}, ${UTILS.random(0, 255)}, ${UTILS.random(0, 255)})`;
-        shapeElement.style.position = 'absolute'
         shapeElement.style.top = `${UTILS.random(0, 70)}%`
         shapeElement.style.left = `${UTILS.random(0, 70)}%`
 
-        shapeInnerElement.style.width = `50%`
-        shapeInnerElement.style.height = `50%`
+        shapeInnerElement.classList = 'shape__inner'
         shapeInnerElement.style.borderRadius = `${UTILS.random(0, 50)}%`
         shapeInnerElement.style.backgroundColor = `RGB(${UTILS.random(0, 255)}, ${UTILS.random(0, 255)}, ${UTILS.random(0, 255)})`;
-        shapeInnerElement.style.position = 'absolute'
-        shapeInnerElement.style.top = "50%"
-        shapeInnerElement.style.left = "50%"
-        shapeInnerElement.style.transform = "translate(-50%, -50%) rotate(45deg)"
         shapeElement.append(shapeInnerElement)
         document.body.append(shapeElement)
-
     }
 }
