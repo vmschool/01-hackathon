73a806989e301ada70d5fa2f364b8adeba935d94
Sergey Fomin <gray7410@yandex.ru>
2022-03-13 00:27:20 +0500

    add russian messages

--- src/context_menu/modules/custom_text.module.js
+++ src/context_menu/modules/custom_text.module.js
@@ -11,7 +11,7 @@ export class CustomText extends Module {
     trigger() {
         return new Promise(() => {
             const menuItem = document.querySelector("[data-type = 'customText']");
-            const POSTS_URL = 'https://jsonplaceholder.typicode.com/posts'
+            const POSTS_URL = 'https://my-json-server.typicode.com/Oleg86rus/01-hackathon/custom-message'
             menuItem.addEventListener('click', () => {
                 const createPostItem = text => {
                     const blockItem = document.createElement('div');
@@ -20,15 +20,15 @@ export class CustomText extends Module {
                     blockItem.innerText = text
                     setTimeout(function(){
                         blockItem.remove()
-                    }, UTILS.random(1000, 5000))
+                    }, UTILS.random(3000, 8000))
                 }
                 const getPost = async () => {   
                     try {
-                        const generatePost = await fetch(`${POSTS_URL}/${UTILS.random(1, 100)}`)
+                        const generatePost = await fetch(`${POSTS_URL}/${UTILS.random(1, 11)}`)
                         console.log(`generatePost`, generatePost)
                         const result = await generatePost.json()
                         console.log('result', result)
-                        const postHTML = createPostItem(result.body)
+                        const postHTML = createPostItem(result.text)
                     } catch (err) {
                         console.error(err)
                     }  
