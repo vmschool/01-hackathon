eca0d7341cfa6a606891555ab747f7e1907d0db0
Oksana Gerasymenko <geroks155@gmail.com>
2022-03-13 09:58:02 +0200

    develop context menu and start in app

--- src/app.js
+++ src/app.js
@@ -1,2 +1,16 @@
 import './styles.css'
+import {ContextMenu} from './menu'
+import {BackgroundModule} from './modules/background.module'
+import {BackgroundGradientModule} from './modules/background-gradient'
+import {ShapeModule} from './modules/shape.module'
+import {ClicksModule} from './modules/clicks.module'
 
+const moduleColor = new BackgroundModule();
+const moduleGradientColor = new BackgroundGradientModule();
+const moduleClick = new ClicksModule();
+const moduleShape = new ShapeModule();
+
+const menu = new ContextMenu('#menu');
+menu.add(moduleClick, moduleShape, moduleColor, moduleGradientColor);
+
+menu.start();
--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,51 @@
 import {Menu} from './core/menu'
+import {Module} from './core/module'
 
 export class ContextMenu extends Menu {
+    #modules;
+
+    constructor(selector) {
+        super(selector);
+        this.#modules = [];
+    }
+
+    open() {
+        if (this.#modules.length === 0) return;
+        this.el.classList.add('open');
+    }
+
+    close() {
+        this.el.classList.remove('open');
+    }
+
+    add(module, ...rest) {
+        const arrModules = [module, ...rest];
+        arrModules.forEach( module => {
+            if (!(module instanceof Module) || this.#modules.includes(module) ) return;
+            this.el.innerHTML += module.toHTML();
+            this.#modules.push(module);
+        })
+    }
+
+    start() {
+        this.el.addEventListener('click', (event) => {
+            if (event.target.parentNode === this.el) {
+                const currentType = event.target.dataset.type;
+                const currentModule = this.#modules.find(module => module.type === currentType);
+                currentModule.trigger();
+                console.log(currentModule)
+            }
+        })
+
+        document.body.addEventListener('contextmenu', (event) => {
+            event.preventDefault();
+            if (this.el.classList.contains('open')) {
+                this.close();
+            }
+            this.el.style.left = event.clientX + 'px'
+            this.el.style.top = event.clientY + 'px';
+            this.open();
+        })
+    }
 
 }
