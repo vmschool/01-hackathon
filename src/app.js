397bf18f85744585794842bde149eef041836e9d
Alex <keeper.gen@gmil.com>
2022-03-13 16:46:17 +0200

    refactored lines in app.js file

--- src/app.js
+++ src/app.js
@@ -4,15 +4,12 @@ import './randomPhotoModule.css'
 import { BackgroundModule } from './modules/background.module';
 import { ClicksModule } from './modules/clicks.module';
 import {RandomPhotoModule} from './modules/show.random.photo.module';
+import { RandomSoundModule } from './modules/randomsound.module'
 
 let bgChange = new BackgroundModule('backgroundChange', 'Замена фона');
 let clickModule = new ClicksModule('clicksCounting', 'Подсчет кликов');
 let showRandomPhoto = new RandomPhotoModule('showRandomPhoto', 'Показать рандомное фото');
+let randomSoundModule = new RandomSoundModule('sound', 'Случайный звук');
 
 showRandomPhoto.trigger();
-
-
-import { RandomSoundModule } from './modules/randomsound.module'
-
-const randomSoundModule = new RandomSoundModule('sound', 'Случайный звук');
 randomSoundModule.trigger();
