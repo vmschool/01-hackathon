3802a9e63f57163e398dc2307a87ec85e872f187
unknown <oskold1979@gmail.com>
2022-03-13 16:52:43 +0300

    Implemented modules bg clicks rndPhoto

--- src/app.js
+++ src/app.js
@@ -1,2 +1,14 @@
-import './styles.css'
+import './styles.css';
+import './randomPhotoModule.css'
+
+import { BackgroundModule } from './modules/background.module';
+import { ClicksModule } from './modules/clicks.module';
+import {RandomPhotoModule} from './modules/show.random.photo.module';
+
+let bgChange = new BackgroundModule('backgroundChange', 'Замена фона');
+let clickModule = new ClicksModule('clicksCounting', 'Подсчет кликов');
+let showRandomPhoto = new RandomPhotoModule('showRandomPhoto', 'Показать рандомное фото');
+
+showRandomPhoto.trigger();
+
 
