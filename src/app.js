61cde6c2d43f081c8049c9ad8e5eb0c5450cd42c
KeepGen <keeper.gen@gmail.com>
2022-03-13 16:41:55 +0200

    Optimized Random Sound Module as Class and added import in app.js file

--- src/app.js
+++ src/app.js
@@ -1,2 +1,6 @@
 import './styles.css'
 
+import { RandomSoundModule } from './modules/randomsound.module'
+
+const randomSoundModule = new RandomSoundModule('sound', 'Случайный звук');
+randomSoundModule.trigger();
