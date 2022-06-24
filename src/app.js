17c2e5f833de5e8768bb4139e866ca8d3ae337a9
Nikita Demyanenko <nikitademyanenko000@gmail.com>
2022-06-25 02:50:06 +1000

    default-main

--- src/app.js
+++ src/app.js
@@ -1,12 +1,1 @@
 import "./styles.css";
-import ContextMenu from "./menu";
-
-const contextMenuItems = [
-  "Фон",
-  "Отлавливание кликов",
-  "Аналитика кликов",
-  "Таймер отсчетов",
-  "Случайная фигура",
-];
-const menu = new ContextMenu("body", ".menu", contextMenuItems);
-menu.render();
--- src/menu.js
+++ src/menu.js
@@ -1,26 +1,3 @@
 import { Menu } from "./core/menu";
 
-export class ContextMenu extends Menu {
-  constructor(el, menuList, menuItems) {
-    super(el);
-    this.menuList = document.querySelector(menuList);
-    this.menuItems = menuItems;
-    this.menuItems.forEach((item) => {
-      const menuItem = document.createElement("li");
-      menuItem.className = "menu-item";
-      menuItem.innerHTML = item;
-      this.menuList.append(menuItem);
-    });
-  }
-
-  render() {
-    this.el.addEventListener("contextmenu", (event) => {
-      event.preventDefault();
-      console.log(event);
-      this.menuList.style.left = `${event.pageX}px`;
-      this.menuList.style.top = `${event.pageY}px`;
-    });
-  }
-}
-
-export default ContextMenu;
+export class ContextMenu extends Menu {}
