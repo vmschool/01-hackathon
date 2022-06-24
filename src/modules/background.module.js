b3a708e70135adaa7ef7a5f9570ae77c42b91376
Cojocaru Artiom <artikocon@gmail.com>
2022-06-24 19:11:51 +0300

    This is a try to set a code for background color

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,5 +1,27 @@
-import {Module} from '../core/module'
+import { Module } from "../core/module";
+import { random } from "../utils";
 
 export class BackgroundModule extends Module {
+  constructor(type, text) {
+    super(type, text);
+  }
+  toHTML() {
+    const uList = document.querySelector(".menu");
+    const newElement = uList.insertAdjacentHTML(
+      "afterend",
+      `<li id="color" class="menu-item" data-type="${this.type}">${this.text}</li>`
+    );
+    return newElement;
+  }
+  setColor() {
+    return random(100, 255);
+  }
 
+  trigger() {
+    const btn = document.querySelector("#color");
+    btn.addEventListener("click", () => {
+      document.body.style.backgroundColor =
+        "rgb(" + setColor() + "," + setColor() + "," + setColor() + ")";
+    });
+  }
 }
