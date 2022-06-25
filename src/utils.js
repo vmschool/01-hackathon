a198839ee787de2996e620b32bd59d5922467723
Roman Burkeev <raburkeev@gmail.com>
2022-06-25 15:05:57 +0300

    add BackgroundModul and getRandomColor function

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,13 @@
 export function random(min, max) {
   return Math.round(min - 0.5 + Math.random() * (max - min + 1))
 }
+
+export const getRandomColor = () => {
+  const letters = 'ABCDEF1234567890'
+  let color = '#'
+  for(let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)]
+  }
+
+  return color
+}
