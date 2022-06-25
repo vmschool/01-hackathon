51ca9f4045c2069b33da9bc8434b72b58bb6c911
unknown <nesquik1723@gmail.com>
2022-06-25 18:53:54 +0300

    backgroundModule

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,5 +1,15 @@
 import {Module} from '../core/module'
 
 export class BackgroundModule extends Module {
+  constructor(type, text) {
+    super(type, text)
+  }
 
+  trigger() { 
+  const buttonColor = document.querySelector('.backgroundSwitcher')
+
+  buttonColor.addEventListener('click', () => {
+    document.body.style.background = '#' + Math.floor(Math.random()*16777215).toString(16)
+  })
+  }
 }
