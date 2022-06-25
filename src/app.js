b53f475e64d3e24107e29cd0471320470d3f38bf
Гуськов Владимир <raz3r444uk@mail.ru>
2022-06-25 15:13:18 +0400

    123

--- src/app.js
+++ src/app.js
@@ -1,17 +1,23 @@
-import './styles.css'
-import { ContextMenu } from './menu';
-import { TimerModule } from './modules/timer.module';
+import "./styles.css";
 
-const contextMenu = new ContextMenu('ul');
-const timerModule = new TimerModule('timer', 'Обратный отсчёт');
+import { ContextMenu } from "./menu";
+// import { TimerModule } from "./modules/timer.module";
+import { ClicksModule } from "./modules/clicks.module";
+
+const contextMenu = new ContextMenu("ul");
+// const timerModule = new TimerModule("timer", "Обратный отсчёт");
+const clicks = new ClicksModule("clicker", "Счетчик сликов");
 contextMenu.add(timerModule);
 
-document.body.addEventListener('contextmenu', (event) => {
-    event.preventDefault();
-    let elemX = document.elementFromPoint(event.clientX + 150, event.clientY);
-    let elemY = document.elementFromPoint(event.clientX, event.clientY + 50);
-    elemX === null ? contextMenu.el.style.left = `${event.clientX - 150}px` : contextMenu.el.style.left = `${event.clientX}px`;
-    elemY === null ? contextMenu.el.style.top = `${event.clientY - 50}px` : contextMenu.el.style.top = `${event.clientY}px`;
-    contextMenu.open();
-})
-
+document.body.addEventListener("contextmenu", (event) => {
+  event.preventDefault();
+  let elemX = document.elementFromPoint(event.clientX + 150, event.clientY);
+  let elemY = document.elementFromPoint(event.clientX, event.clientY + 50);
+  elemX === null
+    ? (contextMenu.el.style.left = `${event.clientX - 150}px`)
+    : (contextMenu.el.style.left = `${event.clientX}px`);
+  elemY === null
+    ? (contextMenu.el.style.top = `${event.clientY - 50}px`)
+    : (contextMenu.el.style.top = `${event.clientY}px`);
+  contextMenu.open();
+});
