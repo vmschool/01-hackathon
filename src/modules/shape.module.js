c897dc77091e6a160c2c9f3ee30e0bc293c24ac6
kolka72 <kolia199972@gmail.com>
2022-06-25 19:09:55 +0300

    refine shape module

--- src/modules/shape.module.js
+++ src/modules/shape.module.js
@@ -10,15 +10,21 @@ export class ShapeModule extends Module {
 
   trigger() {
     document.body.append(this.#shapeElement)
+
     const shapeHeight = random(50, 300)
     const shapeWidth = random(50, 300)
+    
     this.#shapeElement.style.height = shapeHeight + 'px'
     this.#shapeElement.style.width = shapeWidth + 'px'
     this.#shapeElement.style.background = getRandomColor()
     this.#shapeElement.style.borderRadius = random(0, 100) + '%'
     this.#shapeElement.style.position = 'absolute'
-
     this.#shapeElement.style.top = random(0, (document.documentElement.clientHeight - shapeHeight)) + 'px'
     this.#shapeElement.style.left = random(0, (document.documentElement.clientWidth - shapeWidth)) + 'px'
+
+    const timer = setTimeout(() => {
+      this.#shapeElement.remove()
+      clearTimeout(timer)
+    }, 2500)
   }
 }
