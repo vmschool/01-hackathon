ff59917ccb0e8e39611380b83bf2841a493cfda7
Nikita Demyanenko <nikitademyanenko000@gmail.com>
2022-06-25 14:20:30 +0300

    updated dev

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
 import "./styles.css";
 import ContextMenu from "./menu";
 import { Module } from "./core/module";
+import { ClicksModule } from "./modules/clicks.module";
+import "bootstrap/dist/css/bootstrap.css";
 
 const contextMenuItems = [
-  { name: "Аналитика кликов", id: 1 },
-  { name: "Случайная фигура", id: 2 },
-  { name: "Случайный фон", id: 3 },
-  { name: "Случайный звук", id: 4 },
-  { name: "Кастомное сообщение", id: 5 },
-  { name: "Таймер отсчета", id: 6 },
+  { name: "Случайная фигура", id: "1" },
+  { name: "Аналитика кликов", id: "2" },
+  { name: "Случайный фон", id: "3" },
+  { name: "Случайный звук", id: "4" },
+  { name: "Кастомное сообщение", id: "5" },
+  { name: "Таймер отсчета", id: "6" },
 ];
 
 contextMenuItems.forEach((item) => {
@@ -21,3 +23,7 @@ contextMenuItems.forEach((item) => {
 });
 
 const menu = new ContextMenu("body", ".menu", contextMenuItems);
+
+const clickItem = contextMenuItems.find((item) => item.id === "2");
+const clicksModule = new ClicksModule(".menu", clickItem);
+clicksModule.trigger();
--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -1,60 +1,109 @@
 import { Module } from "../core/module";
 
 export class ClicksModule extends Module {
-  constructor(clickElement) {
-    super("1", "1");
-    this.clickElement = document.querySelector(clickElement);
+  constructor(itemsList, item) {
+    super(item.id, item.name);
+    this.itemsList = document.querySelector(itemsList);
+    this.item = item;
   }
 
-  render() {
-    this.clickElement.addEventListener("click", (event) => {
-      if (event.target.textContent === "Отлавливание кликов") {
+  handleDeleteWindow(modalWindow) {
+    modalWindow.style.display = "none";
+  }
+
+  getRandomColor() {
+    const colors = [
+      "skyblue",
+      "yellow",
+      "green",
+      "#c1c1c1",
+      "orange",
+      "lime",
+      "aqua",
+      "fuchsia",
+    ];
+    const random = Math.floor(Math.random() * colors.length);
+    return colors[random];
+  }
+
+  getAnimation() {
+    const animationBlock = document.createElement("div");
+    animationBlock.className = "clicks-timer-element";
+    const animationBlockTime = document.createElement("h1");
+    animationBlockTime.className = "timer-name-head";
+    animationBlock.append(animationBlockTime);
+    return animationBlock;
+  }
+
+  trigger() {
+    this.itemsList.addEventListener("click", (event) => {
+      const { target } = event;
+
+      if (target.dataset.type === this.item.id) {
+        // Диалоговое окно
         const modalWindow = document.createElement("div");
         modalWindow.className = "modal";
         modalWindow.innerHTML = `
-      <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title">Modal title</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-          <p class="modal-body-text"></p>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary close-button" data-bs-dismiss="modal">Close</button>
-        </div>
-      </div>
-    </div>`;
+          <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title">Количество кликов</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <p class="modal-body-text"></p>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-primary close-button" data-bs-dismiss="modal">Закрыть</button>
+            </div>
+          </div>
+        </div>`;
         document.body.prepend(modalWindow);
 
-        const modalBodyText = document.querySelector(".modal-body-text");
+        const modalWindowBodyText = document.querySelector(".modal-body-text");
 
-        const btnClose = document.querySelector(".close-button");
+        // Реализация закрытия окна
+        document.body
+          .querySelector(".close-button")
+          .addEventListener("click", () =>
+            this.handleDeleteWindow(modalWindow)
+          );
 
-        btnClose.addEventListener("click", (e) => {
-          modalWindow.style.display = "none";
-        });
+        document.body
+          .querySelector(".btn-close")
+          .addEventListener("click", () =>
+            this.handleDeleteWindow(modalWindow)
+          );
 
         let clicksLenght = 0;
-        function handleClicksIncrement(el) {
+        // Функция которая помогает нам считать клики
+        function handleClicksIncrement() {
           clicksLenght += 1;
         }
         let timeOfClicks = prompt("Введите количество секунд");
+
+        const animationBlock = this.getAnimation();
+
         timeOfClicks = Number(timeOfClicks);
-        if (timeOfClicks) {
+        if (!isNaN(timeOfClicks) && timeOfClicks > 0) {
+          document.body.prepend(animationBlock);
           document.body.addEventListener("click", handleClicksIncrement);
           const newInterval = setInterval(() => {
             if (timeOfClicks) {
               timeOfClicks -= 1;
-              console.log(timeOfClicks);
+              animationBlock.style.backgroundColor = this.getRandomColor();
+              animationBlock.querySelector("h1").textContent = timeOfClicks;
             } else {
               clearInterval(newInterval);
               document.body.removeEventListener("click", handleClicksIncrement);
               modalWindow.style.display = "block";
-              modalBodyText.textContent = clicksLenght;
+              modalWindowBodyText.textContent = clicksLenght;
+              document.body.querySelector(".clicks-timer-element").remove();
             }
           }, 1000);
+        } else {
+          modalWindow.style.display = "block";
+          modalWindowBodyText.textContent = "Необходимо ввести число!";
         }
       }
     });
