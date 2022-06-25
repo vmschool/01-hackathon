a9568b0b04be359f932eb81080c9abcc8de1c2a5
kolka72 <kolia199972@gmail.com>
2022-06-25 15:42:53 +0300

    modified

--- src/app.js
+++ src/app.js
@@ -1,10 +1,7 @@
 import './styles.css'
 import { ContextMenu } from './menu'
-import { ShapeModule } from './modules/shape.module'
-
-const shapeModule = new ShapeModule('shape', 'Создать фигуру')
 
 const menu = new ContextMenu('#menu', [
-  shapeModule
+
 ])
 menu.render()
