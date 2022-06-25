161e1b5ef4a4600397a64e969622c2a1b91fea4a
Pavel <M3modelist@gmail.com>
2022-06-25 19:42:35 +0300

    delete unusable lines

--- src/js/utils.js
+++ src/js/utils.js
@@ -6,13 +6,3 @@ export function getRandomStringRGB() {
   return `rgb(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)})`;
 }
 
-// const getRandomColor = () =>
-//   '#' +
-//   Math.floor(Math.random() * 0xffffff)
-//     .toString(16)
-//     .padEnd(6, '0')
-//
-// const randomNumber = (min, max) =>
-//   Math.floor(Math.random() * (max - min + 1)) + min
-//
-// const coordinate = randomNumber(5, 20)
