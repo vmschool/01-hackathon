8207e37aa74ab799445d38750c98fa42749f43a6
Anna Olhovskaya <olhovskaya88@gmail.com>
2022-06-25 18:26:56 +0300

    correct randomsound.module.js fole

--- src/modules/randomsound.module.js
+++ src/modules/randomsound.module.js
@@ -1,20 +1,26 @@
 import { Module } from "../core/module";
 
 export class RandomSound extends Module {
-  #sounds = ["eminem", "goodluck", "maksim", "ofcourse", "sweeties", "vladilen"];
+  #sounds = [
+    "eminem",
+    "goodluck",
+    "maksim",
+    "ofcourse",
+    "sweeties",
+    "vladilen",
+  ];
   constructor() {
     super("sound", "sound");
   }
 
   trigger() {
-    const randomSound = this.#sounds[Math.floor(Math.random() * sounds.length)];
+    const randomSound =
+      this.#sounds[Math.floor(Math.random() * this.#sounds.length)];
     this.#playRandomSound(randomSound);
   }
 
   #playRandomSound(audiofile) {
-    const randomSoundAudioFile = new Audio(
-      `../../assets/sounds/${audiofile}.ogg`
-    );
+    const randomSoundAudioFile = new Audio(`/assets/sounds/${audiofile}.ogg`);
     randomSoundAudioFile.autoplay = true;
 
     return randomSoundAudioFile;
