55f104d01909a9c55dba87458b5dd38b465c1545
sjivanov <sjivanov@yandex.ru>
2022-06-25 14:55:59 +0300

    Меню+ background color

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,5 +1,13 @@
 import {Module} from '../core/module'
+import {getRandomColor} from '../utils'
 
 export class BackgroundModule extends Module {
+    constructor (type, text) {
+        super(type, text);
+    }
 
-}
+    trigger(){
+        document.body.style.background = getRandomColor();
+    }
+
+} 
