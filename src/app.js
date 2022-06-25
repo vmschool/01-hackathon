ade091cf5d84e9a00a061150a95cfb8225d7c734
Mikhail Yanzhura <yanzhura@gmail.com>
2022-06-25 12:31:03 +0300

    Добавлен метод add()

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
-import './styles.css'
-import ContextMenu from './menu'
+import './styles.css';
+import ContextMenu from './menu';
+
+import Mod1 from './modules/mod1';
+import Mod2 from './modules/mod2';
+import Mod3 from './modules/mod3';
 
 // const CONTEXT_MENU_EL = document.querySelector('#menu')
 
-const contextMenu = new ContextMenu('#menu')
-contextMenu.add('module1', 'module2', 'module3', 'module4')
+const contextMenu = new ContextMenu('#menu');
+contextMenu.add(new Mod1, new Mod2, new Mod3);
--- src/menu.js
+++ src/menu.js
@@ -7,24 +7,29 @@ export default class ContextMenu extends Menu {
             event.preventDefault();
             if (event.target.offsetParent !== this.el) {
                 this.open(event.offsetX, event.offsetY, this.el);
-              }
+            }
         });
+        this.el,
+            addEventListener('click', event => {
+                console.log(event.target.dataset.type);
+            });
+        this.modulesList = {};
     }
 
     open(offsetX, offsetY, element) {
-        element.setAttribute('style', `top:${offsetY}px; left:${offsetX}px;`)
-        element.classList.add('open')
+        element.setAttribute('style', `top:${offsetY}px; left:${offsetX}px;`);
+        element.classList.add('open');
     }
-    
+
     close() {
-        this.el.classList.remove('open')
+        this.el.classList.remove('open');
     }
 
     add(...modules) {
         modules.forEach(module => {
-            this.el.insertAdjacentHTML('beforeend', `
-                <li class="menu-item">${module}</li>
-            `)
-        })
+            this.modulesList[module.type] = module;
+            this.el.insertAdjacentHTML('beforeend', module.toHTML());
+        });
+        console.log(this.modulesList);
     }
 }
