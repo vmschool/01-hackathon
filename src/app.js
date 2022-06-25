eda4dd728d508ef083d298216172aa3e263b014b
Tai_SaWFly <vawer24@gmail.com>
2022-06-25 22:49:55 +0500

    fix-bugs

--- src/app.js
+++ src/app.js
@@ -2,7 +2,9 @@ import "./styles.css";
 
 import { ContextMenu } from "./menu";
 import { TestModule } from "./modules/test.module";
+import { RandomFigure } from "./modules/randome.figure";
 
 const contextMenu = new ContextMenu();
 
 contextMenu.add(new TestModule());
+contextMenu.add(new RandomFigure());
