12227b4e9f0129f4b22922f564545e5f7d00a325
Igor Sereginn <motivashkin@gmail.com>
2022-06-25 19:39:58 +0300

    add /sound export module sound to app

--- src/modules/sound.module.js
+++ src/modules/sound.module.js
@@ -1,28 +1,12 @@
 import {Module} from '../core/module'
-export class ClicksModule extends Module {
+export class Soundmodule extends Module {
 
-triger() {
-    // создание кнопки Play и встраивание элемента audio в body
-let playAudio = document.createElement('audio');
-let playAudioButton = document.createElement('input');
-playAudioButton.type="button";
-playAudioButton.value = "PLAY";
-let audio = document.querySelector('.audio');
-audio.append(playAudio);
-audio.append(playAudioButton);
-
-let play = document.querySelector('input');
-play.addEventListener('click', (event) => {
-    if(event) {
+trigger() {
         // Выбор случайного аудиофайла
         let random = Math.floor(Math.random() * 10 + 1);
         let randomAudio = new Audio();
-        randomAudio.src = `src/sound/${random}.wav`;
+        randomAudio.src = `src/assets/sound/${random}.wav`;
         randomAudio.play()
         };
-    });
-};
-};
+    };
 
-const sound = new Modules;
-sound.triger();
