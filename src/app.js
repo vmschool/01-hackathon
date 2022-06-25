2c973db431f73630d34307a8f01076657ef0f20d
Eugene015 <egavriluk@gmail.com>
2022-06-25 14:05:14 +0200

    beta

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 import './styles.css'
 import { ContextMenu } from './menu';
 import { TimerModule } from './modules/timer.module';
+import { CustomMessage } from './modules/custom_message.module';
 
 const contextMenu = new ContextMenu('ul');
 const timerModule = new TimerModule('timer', 'Обратный отсчёт');
+const cMessageModule = new CustomMessage('custom message', 'Кастомное сообщение');
 contextMenu.add(timerModule);
+contextMenu.add(cMessageModule);
 
 document.body.addEventListener('contextmenu', (event) => {
     event.preventDefault();
