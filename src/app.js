6a4eeec46965f561faaa65dd6feeb43f4814cc3d
realing29 <realing29@gmail.com>
2022-06-25 16:01:19 +0500

    create menu class

--- src/app.js
+++ src/app.js
@@ -1,2 +1,8 @@
-import './styles.css'
+import "./styles.css";
 
+import { ContextMenu } from "./menu";
+import { TestModule } from "./modules/test.module";
+
+const contextMenu = new ContextMenu();
+
+contextMenu.add(new TestModule());
