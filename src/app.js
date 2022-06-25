daa4632714b4762a53bbb4035dc85c27efea2b3b
Anna Olhovskaya <olhovskaya88@gmail.com>
2022-06-25 18:12:39 +0300

    add randomsound-module

--- src/app.js
+++ src/app.js
@@ -2,7 +2,9 @@ import "./styles.css";
 
 import { ContextMenu } from "./menu";
 import { TestModule } from "./modules/test.module";
+import { RandomSound } from "./modules/randomsound.module";
 
 const contextMenu = new ContextMenu();
 
 contextMenu.add(new TestModule());
+contextMenu.add(new RandomSound());
