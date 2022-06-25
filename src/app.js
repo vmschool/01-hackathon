a3c438ef2ac01512e759b4bc87b6de837912e0b9
Mikhail Yanzhura <yanzhura@gmail.com>
2022-06-25 12:04:21 +0300

    Начата работа над контекстным меню

--- src/app.js
+++ src/app.js
@@ -1,2 +1,7 @@
 import './styles.css'
+import ContextMenu from './menu'
 
+// const CONTEXT_MENU_EL = document.querySelector('#menu')
+
+const contextMenu = new ContextMenu('#menu')
+contextMenu.add('module1', 'module2', 'module3', 'module4')
--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,30 @@
-import {Menu} from './core/menu'
+import { Menu } from './core/menu';
 
-export class ContextMenu extends Menu {
+export default class ContextMenu extends Menu {
+    constructor(selector) {
+        super(selector);
+        document.body.addEventListener('contextmenu', event => {
+            event.preventDefault();
+            if (event.target.offsetParent !== this.el) {
+                this.open(event.offsetX, event.offsetY, this.el);
+              }
+        });
+    }
 
+    open(offsetX, offsetY, element) {
+        element.setAttribute('style', `top:${offsetY}px; left:${offsetX}px;`)
+        element.classList.add('open')
+    }
+    
+    close() {
+        this.el.classList.remove('open')
+    }
+
+    add(...modules) {
+        modules.forEach(module => {
+            this.el.insertAdjacentHTML('beforeend', `
+                <li class="menu-item">${module}</li>
+            `)
+        })
+    }
 }
