05c3fe94d685a607d64bfa87d492eb9116c912e5
Mikhail Yanzhura <yanzhura@gmail.com>
2022-06-25 16:20:45 +0300

    Закончена работа над модулем котекстного меню

--- src/app.js
+++ src/app.js
@@ -1,11 +1,5 @@
 import './styles.css';
 import ContextMenu from './menu';
 
-import Mod1 from './modules/mod1';
-import Mod2 from './modules/mod2';
-import Mod3 from './modules/mod3';
-
-// const CONTEXT_MENU_EL = document.querySelector('#menu')
-
 const contextMenu = new ContextMenu('#menu');
-contextMenu.add(new Mod1, new Mod2, new Mod3);
+// contextMenu.add(new Mod1, new Mod2, new Mod3);
--- src/menu.js
+++ src/menu.js
@@ -3,22 +3,44 @@ import { Menu } from './core/menu';
 export default class ContextMenu extends Menu {
     constructor(selector) {
         super(selector);
+        this.modulesList = {};
         document.body.addEventListener('contextmenu', event => {
             event.preventDefault();
             if (event.target.offsetParent !== this.el) {
-                this.open(event.offsetX, event.offsetY, this.el);
+                this.open(event, this.el);
             }
         });
         this.el,
             addEventListener('click', event => {
-                console.log(event.target.dataset.type);
+                const moduleType = event.target.dataset.type;
+                if (moduleType) {
+                    this.modulesList[moduleType].trigger();
+                }
             });
-        this.modulesList = {};
     }
 
-    open(offsetX, offsetY, element) {
-        element.setAttribute('style', `top:${offsetY}px; left:${offsetX}px;`);
+    adjustMenuPosition(menuX, menuY, element) {
+        const windowMaxX = window.innerWidth;
+        const windowMaxY = window.innerHeight;
+        const menuHeigth = element.getBoundingClientRect().height;
+        const menuWidth = element.getBoundingClientRect().width;
+        let newMenuX = menuX;
+        let newMenuY = menuY;
+        if (menuX + menuWidth > windowMaxX) {
+            newMenuX = windowMaxX - menuWidth - 10;
+        }
+        if (menuY + menuHeigth > windowMaxY) {
+            newMenuY = windowMaxY - menuHeigth - 10;
+        }
+        element.setAttribute('style', `top:${newMenuY}px; left:${newMenuX}px;`);
+    }
+
+    open(event, element) {
+        const menuX = event.offsetX;
+        const menuY = event.offsetY;
+        element.setAttribute('style', `top:${menuY}px; left:${menuX}px;`);
         element.classList.add('open');
+        this.adjustMenuPosition(menuX, menuY, element);
     }
 
     close() {
@@ -30,6 +52,5 @@ export default class ContextMenu extends Menu {
             this.modulesList[module.type] = module;
             this.el.insertAdjacentHTML('beforeend', module.toHTML());
         });
-        console.log(this.modulesList);
     }
 }
