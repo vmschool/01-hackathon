fffbe590a083f622431f8caa26d21b22bb44deba
AlexDyatlov <alexdyatlov.web@gmail.com>
2022-06-25 19:32:05 +0300

    Implemented background module

--- src/app.js
+++ src/app.js
@@ -1,2 +1,4 @@
 import './styles.css'
+import BackgroundModule from './modules/background.module';
 
+const randomColorBg = new BackgroundModule('background', 'Случайный фоновый цвет');
--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,5 +1,30 @@
-import {Module} from '../core/module'
+import { Module } from '../core/module';
+import { random } from '../utils';
 
-export class BackgroundModule extends Module {
+export default class BackgroundModule extends Module {
+	#colorNumber;
 
+	constructor(type, text) {
+		super(type, text);
+		this.#colorNumber = 0;
+	}
+
+	trigger() {
+		let hexString = "0123456789abcdef";
+
+		let randomColor = () => {
+			let hexCode = "#";
+			for (let i = 0; i < 6; i++) {
+				hexCode += hexString[Math.floor(Math.random() * hexString.length)];
+			}
+			return hexCode;
+		}
+
+		let colorOne = randomColor();
+		let colorTwo = randomColor();
+	
+		document.body.style.background = `
+			linear-gradient(${random([this.#colorNumber], 255)}deg, ${colorOne}, ${colorTwo})
+		`;
+	}
 }
