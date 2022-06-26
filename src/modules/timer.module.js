eaa7ec97b618090c88daf7e3cd81fca85c23d79f
Anton Lebedev <sorrint@gmail.com>
2022-06-26 17:53:11 +0800

    func timer

--- src/modules/timer.module.js
+++ src/modules/timer.module.js
@@ -12,6 +12,12 @@ export class TimerModule extends Module {
     #color
     #image
     #userInput
+    #limitMessage
+    #timerSpan
+    #eventContainer
+    #increaseTimer
+    #decreaseTimer
+    #confirmButton
 
     constructor(type, text) {
         super(type, text)
@@ -22,28 +28,33 @@ export class TimerModule extends Module {
         this.#seconds;
         this.#color = randomColor();
         this.#image = document.createElement('img');
+        this.#limitMessage = document.createElement('span');
+        this.#timerSpan = document.createElement('span');
+        this.#eventContainer;
+        this.#increaseTimer = document.createElement('div');
+        this.#decreaseTimer = document.createElement('div');
+        this.#confirmButton = document.createElement('div');
     }
 
     trigger() {
+        this.#eventContainer = document.querySelector(`.${this.type}`)
+        if (!this.#eventContainer) {
+            addEventContainer(this.type);
+            this.#eventContainer = document.querySelector(`.${this.type}`)
+            // console.log(this.#eventContainer)
+            // this.#renderUserInput();
+            // this.#renderTimer();
+            // this.#decreaseTime();
+        }
         this.#renderUserInput();
-        // const eventContainer = document.querySelector(`.${this.type}`);
-        // if (!eventContainer) {
-        // addEventContainer(this.type);
-        // this.#render();
-        // this.#decreaseTime();
-        // }
-        // addEventContainer();
-        // this.#renderUserInput();
-        // if (this.#timerText.textContent === 'time is up') {
-        //     this.#decreaseTime();
-        // }
     }
 
-    #render() {
-        addEventContainer(this.type);
-        const eventContainer = document.querySelector(`.${this.type}`);
-        console.log(eventContainer);
-        eventContainer.style.background = `black`;
+    #renderTimer() {
+        this.#userInput.style.display = 'none';    
+
+        this.#minutes = 0;
+        this.#seconds = this.#timerSpan.textContent;
+        this.#eventContainer.style.background = `black`;
 
         this.#timerWindow.className = 'timer-window';
         this.#timerWindow.style.border = `1px solid ${this.#color}`;
@@ -58,12 +69,13 @@ export class TimerModule extends Module {
         this.#image.src = JS_MEME;
 
         this.#timerWindow.append(this.#timerText);
-        eventContainer.append(this.#timerWindow, this.#image);
+        this.#eventContainer.append(this.#timerWindow, this.#image);
+        this.#decreaseTime();
     }
 
     #decreaseTime() {
         this.#image.classList.add('hidden');
-        this.#seconds = '5';
+        
         let time = setInterval(() => {
             if (this.#seconds <= 0) {
                 clearInterval(time);
@@ -78,42 +90,71 @@ export class TimerModule extends Module {
     }
 
     #renderUserInput() {
-        addEventContainer();
-        let eventContainer = document.querySelector(`.${this.type}`);
-        if (!eventContainer) {
-            addEventContainer(this.type);
-            eventContainer = document.querySelector(`.${this.type}`);
-        }
         this.#userInput.className = 'user-input';
+        this.#applySytles(this.#userInput);
+
+        this.#increaseTimer.className = 'user-button increase';
+        this.#increaseTimer.textContent = '+5';
+        this.#applySytles(this.#increaseTimer);
+
+        this.#decreaseTimer.className = 'user-button decrease';
+        this.#decreaseTimer.textContent = '-5';
+        this.#applySytles(this.#decreaseTimer)
+
+        this.#timerSpan.className = 'timer-span';
+        this.#timerSpan.textContent = '60';
+
+        this.#confirmButton.className = 'user-button confirm';
+        this.#confirmButton.textContent = 'Start';
+        this.#applySytles(this.#confirmButton);
+
+        this.#limitMessage.className = 'limit-message';
 
 
-        const increaseTimer = document.createElement('div');
-        increaseTimer.className = 'increase';
-        increaseTimer.classList.add('user-button');
-        increaseTimer.textContent = '+5';
-
-        const decreaseTimer = document.createElement('div');
-        decreaseTimer.className = 'user-button decrease';
-        decreaseTimer.textContent = '-5';
-
-        const timerSpan = document.createElement('span');
-        timerSpan.className = 'timer-span';
-        timerSpan.textContent = '60';
+        this.#userInput.append(this.#increaseTimer, this.#timerSpan, this.#decreaseTimer, this.#confirmButton);
+        this.#eventContainer.append(this.#userInput);
 
         this.#userInput.addEventListener('click', (event) => {
             const { target } = event;
-            if (target.classList.contains("increase")) {
-                console.log(target.className)
+            const currentTime = Number(this.#timerSpan.textContent);
+            if (currentTime === 60 && target.classList.contains("increase")) {
+                this.#renderLimitMessage();
+            }
+            if (currentTime === 0 && target.classList.contains("decrease")) {
+                this.#renderLimitMessage();
+            }
+            if (currentTime >= 0 && currentTime <= 60) {
+                if (target.classList.contains("increase") && currentTime != 60) {
+                    this.#timerSpan.textContent = currentTime + 5;
+                } else if (target.classList.contains("decrease") && currentTime != 0) {
+                    this.#timerSpan.textContent = currentTime - 5;
+                }
+            }
+            if (target.classList.contains('confirm')) {
+                this.#renderTimer();
             }
         })
-        this.#userInput.append(increaseTimer, timerSpan, decreaseTimer);
-        eventContainer.append(this.#userInput);
-        console.log(eventContainer)
+    }
 
+    #renderLimitMessage() {
+        let info = '';
+        if (this.#timerSpan.textContent === "60") {
+            info = 'Это максимальное возможное значение';
+        }
+        if (this.#timerSpan.textContent === "0") {
+            info = 'Это минимальное возможное значение';
+        }
+        this.#limitMessage.textContent = info;
+        this.#eventContainer.append(this.#limitMessage);
+    }
+    #renderInputElement() {
 
     }
 
-    #renderInputElement() {
-
+    #applySytles(element) {
+        element.style.border = `1px solid ${this.#color}`;
+        element.style.boxShadow = `0 0 2px ${this.#color}, 0 0 10px ${this.#color}`;
+        element.style.color = `${this.#color}`;
+        element.style.textShadow = `0 0 2px ${this.#color}, 0 0 5px ${this.#color}`;
     }
 }
