663f03df3a43350d11659c30adc2a5b17f6b492e
Nikita Demyanenko <nikitademyanenko000@gmail.com>
2022-06-26 04:15:41 +0300

    Реализована функция смены цвета

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,18 @@
 export function random(min, max) {
-  return Math.round(min - 0.5 + Math.random() * (max - min + 1))
+  return Math.round(min - 0.5 + Math.random() * (max - min + 1));
+}
+
+export function getRandomColor() {
+  const colors = [
+    "skyblue",
+    "yellow",
+    "green",
+    "#c1c1c1",
+    "orange",
+    "lime",
+    "aqua",
+    "fuchsia",
+  ];
+  const random = Math.floor(Math.random() * colors.length);
+  return colors[random];
 }
