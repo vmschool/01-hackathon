ba1b31b792efe2045afcc764d6d4aefa6bdd2906
Skripnik Vadim <tsermer2015@gmail.com>
2022-06-26 18:05:05 +1000

    Implemented background background module

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,5 +1,19 @@
-import {Module} from '../core/module'
+import { Module } from "../core/module";
 
 export class BackgroundModule extends Module {
+  #body;
+  constructor(type, text) {
+    super(type, text);
+    this.#body = document.querySelector("body");
+  }
 
+  trigger() {
+    this.#body.style.backgroundColor = this.#getRandomColor();
+  }
+
+  #getRandomColor() {
+    return (
+      "#" + (0x1000000 + Math.random() * 0xffffff).toString(16).substring(1, 7)
+    );
+  }
 }
