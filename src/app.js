0b857f42fb51fc216c678926cb43ced65f161001
unknown <spichakandrey79@yandex.ru>
2022-06-26 11:43:54 +0300

    fixes timer

--- src/app.js
+++ src/app.js
@@ -2,12 +2,15 @@ import './styles.css'
 import { ContextMenu } from './menu'
 import { ShapeModule } from './modules/shape.module'
 import { RandomWords } from './modules/text.module'
+import { TimerModule } from './modules/timer.module'
 
 const shapeModule = new ShapeModule('shape', 'Создать фигуру')
 const randomWords = new RandomWords('random', 'Кастомное сообщение')
+const timerModule = new TimerModule('timer', 'Таймер')
 
 const menu = new ContextMenu('#menu', [
   shapeModule,
-  randomWords
+  randomWords,
+  timerModule
 ])
 menu.render()
