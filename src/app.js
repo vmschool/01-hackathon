dc6bc332343a2d195c5b785d375a2f9fb7140b44
realing29 <realing29@gmail.com>
2022-06-26 17:17:20 +0500

    rework module background

--- src/app.js
+++ src/app.js
@@ -4,9 +4,11 @@ import { ContextMenu } from "./menu";
 import { TestModule } from "./modules/test.module";
 import { RandomFigure } from "./modules/randome.figure";
 import { RandomSound } from "./modules/randomsound.module";
+import { BackgroundModule } from "./modules/background.module";
 
 const contextMenu = new ContextMenu();
 
 contextMenu.add(new TestModule());
 contextMenu.add(new RandomFigure());
 contextMenu.add(new RandomSound());
+contextMenu.add(new BackgroundModule());
