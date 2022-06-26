da677671e1a015263bf2ab3fa4e200c90f77be5e
Roman Trutnev <trutenoriginal@gmail.com>
2022-06-26 15:05:49 +0300

    исправлены баги version 2.0

--- src/app.js
+++ src/app.js
@@ -43,23 +43,21 @@ menuList.addEventListener("click", (event) => {
     const randomSound = new RandomSounds("mp3", "audio");
     randomSound.trigger();
   }
-
-
   if (target.dataset.type === "6") {
     const timer = new Timer()
     timer.render()
   }
+
+  if (target.dataset.type === "7") {
+    const randomComment = new RandomComment("comment", "comment");
+    randomComment.trigger();
+  }
+
+  if (target.dataset.type === "8") {
+    const balloon = new Balloon("balloon", "balloon");
+    balloon.trigger();
+  }
 })
-if (target.dataset.type === "7") {
-  const randomComment = new RandomComment("comment", "comment");
-  randomComment.trigger();
-}
-
-if (target.dataset.type === "8") {
-  const balloon = new Balloon("balloon", "balloon");
-  balloon.trigger();
-}
-
 const clickItem = contextMenuItems.find((item) => item.id === "2");
 const clicksModule = new ClicksModule(".menu", clickItem);
 clicksModule.trigger();
